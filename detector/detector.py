6d3a64555c619aba651823cd4d9e970a9ebd1f41
Agustín Azzinnari <agustin@tryolabs.com>
2017-02-22 12:02:17 -0300

    Refactor into separate functions a bit

--- detector/detector.py
+++ detector/detector.py
@@ -11,19 +11,7 @@ from .voc import read_classes
 IMAGE_SIZE = 200
 
 
-@click.command()
-@click.option('--data-dir', default='datasets/voc/')
-@click.option('--log-dir', default='logs/')
-@click.option('--model-dir', default='models/')
-@click.option('--num-epochs', default=5)
-@click.option('--batch-size', default=32)
-@click.option('--print-every', default=5)
-def main(data_dir, log_dir, model_dir, num_epochs, batch_size, print_every):
-    # Save each session log using the date they run.
-    log_dir = os.path.join(
-        log_dir, str(datetime.now()).split('.')[0].replace(' ', '_')
-    )
-
+def inputs(data_dir, num_epochs, batch_size):
     classes = read_classes(data_dir)
 
     train_path = os.path.join(data_dir, 'tf', 'train.tfrecords')
@@ -82,26 +70,21 @@ def main(data_dir, log_dir, model_dir, num_epochs, batch_size, print_every):
     # TODO: With this, are there two or three queues? Is there a queue for the
     # batches or does it end on single examples?
 
-    # Inputs and outputs dimensions.
-    num_classes = len(classes)
+    return image_batch, label_batch
 
-    # Hyperparameters set up.
+
+def inference(X, num_classes, reg):
+    """
+    Build the model for performing inference on input X.
+
+    Adds regularization losses to the TF `losses` collection.
+    """
     hidden_size = 500
     conv_size = [3, 3, 3, 32]
 
-    reg = 1e-4
-    learning_rate = 0.001
-    beta1 = 0.9
-    beta2 = 0.999
-
-    # Inputs to graph.
-    # X = tf.placeholder(tf.float32, shape=[None, width, height, channels])
-    # y_true = tf.placeholder(tf.int64, shape=[None])
-    X = image_batch
-    y_true = label_batch
-
-    # Graph architecture.
     Wconv = tf.Variable(tf.random_normal(conv_size, stddev=0.01))
+    tf.add_to_collection('losses', reg * tf.nn.l2_loss(Wconv))
+
     bconv = tf.Variable(tf.zeros([conv_size[-1]]))
     conv = tf.nn.relu(tf.nn.conv2d(
         X, Wconv,
@@ -124,6 +107,8 @@ def main(data_dir, log_dir, model_dir, num_epochs, batch_size, print_every):
     ]
 
     W1 = tf.Variable(tf.random_normal(hidden_shape, stddev=0.01))
+    tf.add_to_collection('losses', reg * tf.nn.l2_loss(W1))
+
     b1 = tf.Variable(tf.zeros([hidden_size]))
 
     hidden = tf.nn.relu(tf.matmul(
@@ -133,57 +118,69 @@ def main(data_dir, log_dir, model_dir, num_epochs, batch_size, print_every):
     W2 = tf.Variable(
         tf.random_normal([hidden_size, num_classes], stddev=0.01)
     )
+    tf.add_to_collection('losses', reg * tf.nn.l2_loss(W2))
+
     b2 = tf.Variable(tf.zeros([num_classes]))
 
     y_pred = tf.matmul(hidden, W2) + b2
 
-    # Metrics operations.
-    auc, update_auc_op = tf.contrib.metrics.streaming_auc(
-        y_pred, y_true, curve='PR'
-    )
+    return y_pred
 
-    # Data and regularization loss operations.
-    data_loss = tf.reduce_mean(
-        tf.nn.sigmoid_cross_entropy_with_logits(logits=y_pred, labels=y_true)
-    )
 
-    reg_loss = reg * (
-        tf.nn.l2_loss(Wconv) +
-        tf.nn.l2_loss(W1) +
-        tf.nn.l2_loss(W2)
-    )
-
-    loss = data_loss + reg_loss
-
-    # Declare and merge summary values.
-    tf.summary.scalar('loss', loss)
+def metrics(logits, labels):
+    # TODO: Doesn't make much sense over training data, averaging with initial
+    # (bad) predictions.
+    auc, update_auc_op = tf.metrics.auc(labels, logits, curve='PR')
+    tf.add_to_collection('metric_ops', update_auc_op)
+    tf.add_to_collection('metrics', auc)
     tf.summary.scalar('auc', auc)
-    summarizer = tf.summary.merge_all()
 
-    # Training operation; automatically updates all variables using SGD.
+
+def loss(logits, labels):
+    data_loss = tf.reduce_mean(
+        tf.nn.sigmoid_cross_entropy_with_logits(logits=logits, labels=labels)
+    )
+    tf.add_to_collection('losses', data_loss)
+
+    total_loss = tf.add_n(tf.get_collection('losses'))
+    tf.summary.scalar('total_loss', total_loss)
+
+    return total_loss
+
+
+def optimizer(total_loss, learning_rate, beta1, beta2):
     global_step = tf.Variable(0, trainable=False)
     train_op = tf.train.AdamOptimizer(
         learning_rate=learning_rate, beta1=beta1, beta2=beta2
-    ).minimize(loss, global_step=global_step)
+    ).minimize(total_loss, global_step=global_step)
+
+    return global_step, train_op
+
+
+def train(global_step, train_op, total_loss, log_dir, model_dir, print_every):
+    # Merge all summary values.
+    summarizer = tf.summary.merge_all()
 
     # Operation to save and restore variables on the graph.
     saver = tf.train.Saver()
 
     # Create initializer for variables. Queue-related variables need a special
     # initializer.
-    init = tf.group(
+    init_op = tf.group(
         tf.global_variables_initializer(),
         tf.local_variables_initializer()
     )
 
+    metric_ops = tf.get_collection('metric_ops')
+    metrics = tf.get_collection('metrics')
+
     print("graph built, starting the session")
     with tf.Session() as sess:
         # Run the initializer, then the rest.
-        sess.run(init)
+        sess.run(init_op)
 
         # Create seaprate summary writers for training and validation data.
         train_writer = tf.summary.FileWriter(f'{log_dir}/train', sess.graph)
-        # val_writer = tf.summary.FileWriter(f'{log_dir}/val', sess.graph)
 
         # Start queue runner threads.
         coord = tf.train.Coordinator()
@@ -192,44 +189,31 @@ def main(data_dir, log_dir, model_dir, num_epochs, batch_size, print_every):
         print("setup complete, start training")
         try:
             while not coord.should_stop():
-                # Run the training operation.
-                (
-                    _, _, summary, train_loss, train_auc, step
-                ) = sess.run([
-                    train_op, update_auc_op, summarizer, loss, auc, global_step
+                # Run the training operations.
+                _, summary, train_loss, step, *_ = sess.run([
+                    train_op, summarizer, total_loss, global_step, metric_ops
+                ])
+
+                # Run the metric operations.
+                values = sess.run(metrics)
+                metrics_report = ', '.join([
+                    f'{metric.op.name} = {value:.2f}'
+                    for metric, value in zip(metrics, values)
                 ])
 
                 # Get and track metrics for validation and training sets.
                 if step % print_every == 0:
                     train_writer.add_summary(summary, step)
 
-                    # TODO: How to handle validation data?
-                    # feed = {X: data['X_val'], y_true: data['y_val']}
-                    # summary, val_loss, val_acc = sess.run(
-                    #     [summarizer, loss, accuracy],
-                    #     feed_dict=feed
-                    # )
-                    # val_writer.add_summary(summary, idx)
-
-                    line = (
-                        'iter = {}, train_loss = {:.2f}, train_auc = {:.2f}, '
-                        # 'val_loss = {:.2f}, val_acc = {:.2f}'
-                    )
-                    print(line.format(
-                        step, train_loss, train_auc,  # val_loss, val_acc
-                    ))
+                    line = 'iter = {}, loss = {:.2f}, {}'
+                    print(line.format(step, train_loss, metrics_report))
 
                     saver.save(sess, os.path.join(log_dir, 'conv'), step)
 
         except tf.errors.OutOfRangeError:
             if step % print_every != 0:
-                line = (
-                    'iter = {}, train_loss = {:.2f}, train_auc = {:.2f}, '
-                    # 'val_loss = {:.2f}, val_acc = {:.2f}'
-                )
-                print(line.format(
-                    step, train_loss, train_auc,  # val_loss, val_acc
-                ))
+                line = 'iter = {}, train_loss = {:.2f}, {}'
+                print(line.format(step, train_loss), metrics_report)
             print('finished training -- epoch limit reached')
         finally:
             coord.request_stop()
@@ -242,3 +226,61 @@ def main(data_dir, log_dir, model_dir, num_epochs, batch_size, print_every):
         save_path = saver.save(sess, model_dir)
         print()
         print(f'saving result to save_path = {save_path}')
+
+
+@click.command()
+@click.option('--data-dir', default='datasets/voc/')
+@click.option('--log-dir', default='logs/')
+@click.option('--model-dir', default='models/')
+@click.option('--num-epochs', default=5)
+@click.option('--batch-size', default=32)
+@click.option('--reg', default=1e-4)
+@click.option('--learning_rate', default=0.001)
+@click.option('--beta1', default=0.9)
+@click.option('--beta2', default=0.999)
+@click.option('--print-every', default=5)
+def main(data_dir, log_dir, model_dir, num_epochs, batch_size, reg,
+         learning_rate, beta1, beta2, print_every):
+
+    # Save each session log using the date they run.
+    log_dir = os.path.join(
+        log_dir, str(datetime.now()).split('.')[0].replace(' ', '_')
+    )
+
+    # Print selected options for sanity check.
+    print(f"data_dir = {data_dir}")
+    print(f"log_dir = {log_dir}")
+    print(f"model_dir = {model_dir}")
+
+    print(f"num_epochs = {num_epochs}")
+    print(f"batch_size = {batch_size}")
+
+    print(f"reg = {reg}")
+    print(f"learning_rate = {learning_rate}")
+    print(f"beta1 = {beta1}")
+    print(f"beta2 = {beta2}")
+
+    print(f"print_every = {print_every}")
+
+    # Inputs for graph.
+    X, y_true = inputs(data_dir, num_epochs, batch_size)
+
+    # Inputs and outputs dimensions.
+    num_classes = int(y_true.shape[1])
+
+    # Graph architecture.
+    y_pred = inference(X, num_classes, reg)
+
+    # Metrics operations.
+    metrics(y_pred, y_true)
+
+    # Data and regularization loss operations.
+    total_loss = loss(y_pred, y_true)
+
+    # Training operation; automatically updates all variables using SGD.
+    global_step, train_op = optimizer(total_loss, learning_rate, beta1, beta2)
+
+    # Perform the training.
+    train(
+        global_step, train_op, total_loss, log_dir, model_dir, print_every
+    )
