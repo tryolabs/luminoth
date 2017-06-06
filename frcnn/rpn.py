309a2987c28df9cd82da00a47147e6492414e6aa
Javier Rey <javirey@gmail.com>
2017-06-06 11:37:25 -0300

    Adds RPN basic test with session to test dimensions

--- frcnn/rpn.py
+++ frcnn/rpn.py
@@ -1,17 +1,42 @@
+import collections
+import numpy as np
 import sonnet as snt
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
 from sonnet.python.modules.conv import Conv2D
+from utils.generate_anchors import generate_anchors
+
 
 class RPN(snt.AbstractModule):
     def __init__(self, anchor_scales, anchor_ratios, num_channels=512,
                  kernel_shape=[3, 3], is_training=False, name='rpn'):
         """RPN"""
         super(RPN, self).__init__(name=name)
+
+        if not isinstance(anchor_scales, collections.Iterable):
+            raise TypeError("anchor_scales must be iterable")
+        anchor_scales = tuple(anchor_scales)
+
+        if not isinstance(anchor_ratios, collections.Iterable):
+            raise TypeError("anchor_ratios must be iterable")
+        anchor_ratios = tuple(anchor_ratios)
+
+        if not isinstance(kernel_shape, collections.Iterable):
+            raise TypeError("kernel_shape must be iterable")
+        kernel_shape = tuple(kernel_shape)
+
+        if not anchor_scales:
+            raise ValueError("anchor_scales must not be empty")
+        if not anchor_ratios:
+            raise ValueError("anchor_ratios must not be empty")
+
         self._anchor_scales = anchor_scales
         self._anchor_ratios = anchor_ratios
-        self._num_anchors = len(anchor_scales) * len(anchor_ratios)
+        # TODO: Do we need the anchors? Can't we just use len(self._anchor_scales) * len(self._anchor_ratios)
+        self._num_anchors = self.anchors.shape[0]
+
+        print(self.anchors.shape)
 
         self._num_channels = num_channels
         self._kernel_shape = kernel_shape
@@ -27,7 +52,7 @@ class RPN(snt.AbstractModule):
             self._rpn = Conv2D(
                 output_channels=self._num_channels,
                 kernel_shape=self._kernel_shape,
-                initializers={'w': self._initializer}, name='conv'
+                initializers={'w': self._initializer, 'b': self._initializer}, name='conv'
             )
 
             self._rpn_cls = Conv2D(
@@ -44,26 +69,58 @@ class RPN(snt.AbstractModule):
             )
 
     def _build(self, pretrained, is_training=True):
-
+        """
+        TODO: We don't have BatchNorm yet.
+        """
         rpn = self._rpn_activation(
-            self._rpn(pretrained, is_training=is_training))
+            self._rpn(pretrained))
 
-        rpn_cls_score = self._rpn_cls(rpn, is_training=is_training)
-        rpn_cls_score_reshape = self._reshape_layer(
-            rpn_cls_score, 2, 'rpn_cls_score_reshape')
+        rpn_cls_score = self._rpn_cls(rpn)
+        rpn_cls_score_reshape = self.spatial_reshape_layer(rpn_cls_score, 2)
+        rpn_cls_prob = self.spatial_softmax(rpn_cls_score_reshape)
+        rpn_cls_prob_reshape = self.spatial_reshape_layer(rpn_cls_prob, self._num_anchors * 2)
+        rpn_bbox_pred = self._rpn_bbox(rpn)
 
-        # TODO: Malas dimensiones con el reshape de arriba.
-        rpn_cls_prob_reshape = tf.nn.softmax(
-            rpn_cls_score_reshape, name='rpn_cls_prob_reshape')
-        rpn_cls_prob = self._reshape_layer(
-            rpn_cls_prob_reshape, self._num_anchors * 2, 'rpn_cls_prob')
+        return {
+            'rpn': rpn,
+            'rpn_cls_prob': rpn_cls_prob,
+            'rpn_cls_prob_reshape': rpn_cls_prob_reshape,
+            'rpn_cls_score': rpn_cls_score,
+            'rpn_cls_score_reshape': rpn_cls_score_reshape,
+            'rpn_bbox_pred': rpn_bbox_pred,
+        }
 
+    def spatial_softmax(self, input):
+        input_shape = tf.shape(input)
+        reshaped_input = tf.reshape(input, [-1, input_shape[3]])
+        softmaxed = tf.nn.softmax(reshaped_input)
+        return tf.reshape(
+            softmaxed, [-1, input_shape[1], input_shape[2], input_shape[3]]
+        )
 
-        rpn_bbox_pred = self._rpn_bbox(rpn, is_training=is_training)
+    def spatial_reshape_layer(self, input, num_dim):
+        input_shape = tf.shape(input)
+        # transpose: (1, H, W, A x d) -> (1, H, WxA, d)
+        return tf.reshape(
+            input, [
+                input_shape[0],
+                input_shape[1],
+                -1,
+                int(num_dim)
+        ])
 
-        return rpn_cls_prob, rpn_bbox_pred
+    @property
+    def anchors(self):
+        if not hasattr(self, '_anchors') or self._anchors is None:
+            self._anchors = self._generate_anchors()
+        return self._anchors
 
+    def _generate_anchors(self):
+        """
+        Generate anchors based on the ratios and scales.
 
-    def _reshape_layer(self, bottom, num_dim, *args):
-        # TODO: tf-faster-rcnn/lib/network.py
-        raise NotImplemented()
+        We use the original code found in `rbgirshick/py-faster-rcnn`
+        """
+        return generate_anchors(
+            ratios=np.array(self._anchor_ratios), scales=np.array(self._anchor_scales)
+        )
---
+++ frcnn/rpn_test.py
@@ -0,0 +1,82 @@
+import sonnet as snt
+import tensorflow as tf
+import numpy as np
+
+from sonnet.testing.parameterized import parameterized
+
+from rpn import RPN
+
+class RPNTest(parameterized.ParameterizedTestCase, tf.test.TestCase):
+    def setUp(self):
+        super(RPNTest, self).setUp()
+        self.anchor_scales = [8, 16, 32]
+        self.anchor_ratios = [0.5, 1, 2]
+        self.num_channels = 512
+        self.kernel_shape = [3, 3]
+
+    @parameterized.NamedParameters(
+        ('CorrectTest', 1, 2, 1, 4),
+        ('IncorrectTest', 4, 5, 16, 25),
+    )
+    def testSample(self, in1, in2, out1, out2):
+        with self.test_session():
+            x = tf.square([in1, in2])
+            self.assertAllEqual(x.eval(), [out1, out2])
+
+
+    def testConstructor(self):
+        with self.assertRaisesRegexp(TypeError, 'anchor_scales must be iterable'):
+            net = RPN(None, self.anchor_ratios)
+
+        with self.assertRaisesRegexp(TypeError, 'anchor_ratios must be iterable'):
+            net = RPN(self.anchor_scales, None)
+
+        with self.assertRaisesRegexp(ValueError, 'anchor_scales must not be empty'):
+            net = RPN([], self.anchor_ratios)
+
+        with self.assertRaisesRegexp(ValueError, 'anchor_ratios must not be empty'):
+            net = RPN(self.anchor_scales, [])
+
+    def testBasic(self):
+        model = RPN(self.anchor_scales, self.anchor_ratios, self.num_channels, self.kernel_shape)
+        # With an image of (100, 100, 3) we get a VGG output of (32, 32, 512) (plus the batch number)
+        pretrained_output_shape = (1, 32, 32, 512)
+        pretrained_output = tf.placeholder(tf.float32, shape=pretrained_output_shape)
+        layers = model(pretrained_output)
+
+        with self.test_session() as sess:
+            # As in the case of a real session we need to initialize the variables.
+            sess.run(tf.global_variables_initializer())
+            layers_inst = sess.run(layers, feed_dict={pretrained_output: np.random.rand(*pretrained_output_shape)})
+
+        # Since pretrained
+        rpn_shape = layers_inst['rpn'].shape
+        # RPN has the same shape as the pretrained layer.
+        self.assertEqual(pretrained_output_shape, rpn_shape)
+
+        num_anchors = model._num_anchors
+
+        # Class score generates 2 values per anchor
+        rpn_cls_score_shape = layers_inst['rpn_cls_score'].shape
+        rpn_cls_score_true_shape = pretrained_output_shape[:-1] + (num_anchors * 2,)  # num_anchors * 2
+        self.assertEqual(rpn_cls_score_shape, rpn_cls_score_true_shape)
+
+        rpn_cls_score_reshape_shape = layers_inst['rpn_cls_score_reshape'].shape
+        rpn_cls_score_reshape_true_shape = pretrained_output_shape[:-1] + (num_anchors * 2,)  # num_anchors * 2
+        self.assertEqual(rpn_cls_score_reshape_shape, (1, 32, 288, 2))
+
+        # RPN class prob shape has the spatial reshape
+        rpn_cls_prob_shape = layers_inst['rpn_cls_prob'].shape
+        self.assertEqual(rpn_cls_prob_shape, (1, 32, 288, 2))
+
+        rpn_cls_prob_reshape_shape = layers_inst['rpn_cls_prob_reshape'].shape
+        rpn_cls_prob_reshape_true_shape = pretrained_output_shape[:-1] + (num_anchors * 2,)  # num_anchors * 2
+        self.assertEqual(rpn_cls_prob_reshape_shape, rpn_cls_prob_reshape_true_shape)
+
+        rpn_bbox_pred_shape = layers_inst['rpn_bbox_pred'].shape
+        rpn_bbox_pred_true_shape = pretrained_output_shape[:-1] + (num_anchors * 4,)  # num_anchors * 4 (bbox regression)
+        self.assertEqual(rpn_bbox_pred_shape, rpn_bbox_pred_true_shape)
+
+
+if __name__ == "__main__":
+    tf.test.main()
