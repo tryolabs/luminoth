3e68baf79434a18cc62a6ef7479a9e094f24e4f2
Javier Rey <javirey@gmail.com>
2017-07-12 00:27:18 -0300

    Back to using resnet101

--- frcnn/pretrained/resnet_v2.py
+++ frcnn/pretrained/resnet_v2.py
@@ -14,10 +14,10 @@ class ResNetV2(Pretrained):
         is_training = self._trainable and is_training
         inputs = self._preprocess(inputs)
         with arg_scope(resnet_v2.resnet_utils.resnet_arg_scope(is_training=is_training)):
-            net, end_points = resnet_v2.resnet_v2_50(inputs)
+            net, end_points = resnet_v2.resnet_v2_101(inputs)
 
         return {
-            'net': dict(end_points)['fasterrcnn_1/resnet_v2/resnet_v2_50/block2/unit_3/bottleneck_v2/conv3']
+            'net': dict(end_points)['fasterrcnn_1/resnet_v2/resnet_v2_101/block4/unit_3/bottleneck_v2/conv3'],
         }
 
     def _preprocess(self, inputs):
