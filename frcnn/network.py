f9293b469d925052aa328e5c310a748873349e00
Javier Rey <javirey@gmail.com>
2017-07-15 22:40:07 -0300

    Moves pretrained outside network, move submodules to _build and minor training options improvements

--- frcnn/network.py
+++ frcnn/network.py
@@ -2,29 +2,21 @@ import numpy as np
 import sonnet as snt
 import tensorflow as tf
 
-from .dataset import TFRecordDataset
-from .pretrained import VGG, ResNetV2
 from .rcnn import RCNN
 from .roi_pool import ROIPoolingLayer
 from .rpn import RPN
 
-from .utils.generate_anchors import generate_anchors as generate_anchors_reference
+from .utils.generate_anchors import generate_anchors as generate_anchors_ref
 from .utils.ops import meshgrid
-from .utils.image import draw_bboxes, normalize_bboxes
+from .utils.image import draw_bboxes
 from .utils.vars import variable_summaries
-
-
-PRETRAINED_MODULES = {
-    'vgg': VGG,
-    'resnet': ResNetV2,
-    'resnetv2': ResNetV2,
-}
 
 
 class FasterRCNN(snt.AbstractModule):
     """Faster RCNN Network"""
 
-    def __init__(self, config, debug=False, with_rcnn=True, num_classes=None, pretrained_type='vgg', name='fasterrcnn'):
+    def __init__(self, config, debug=False, with_rcnn=True, num_classes=None,
+                 name='fasterrcnn'):
         super(FasterRCNN, self).__init__(name=name)
 
         self._cfg = config
@@ -35,9 +27,10 @@ class FasterRCNN(snt.AbstractModule):
         self._anchor_base_size = self._cfg.ANCHOR_BASE_SIZE
         self._anchor_scales = np.array(self._cfg.ANCHOR_SCALES)
         self._anchor_ratios = np.array(self._cfg.ANCHOR_RATIOS)
-        self._anchor_stride = self._cfg.ANCHOR_STRIDE  # TODO: Value depends on use of VGG vs Resnet (?)
+        # TODO: Value depends on use of VGG vs Resnet (?)
+        self._anchor_stride = self._cfg.ANCHOR_STRIDE
 
-        self._anchor_reference = generate_anchors_reference(
+        self._anchor_reference = generate_anchors_ref(
             self._anchor_base_size, self._anchor_ratios, self._anchor_scales
         )
         self._num_anchors = self._anchor_reference.shape[0]
@@ -47,23 +40,9 @@ class FasterRCNN(snt.AbstractModule):
 
         self._rcnn_cls_loss_weight = 1.0
         self._rcnn_reg_loss_weight = 2.0
+        self._losses_collections = ['fastercnn_losses']
 
-        if pretrained_type not in PRETRAINED_MODULES:
-            raise ValueError(
-                'Invalid type for pretrained module: "{}", should be one of: {}'.format(
-                    pretrained_type, PRETRAINED_MODULES
-                )
-            )
-        self._pretrained_class = PRETRAINED_MODULES[pretrained_type]
-
-        with self._enter_variable_scope():
-            self._pretrained = self._pretrained_class(trainable=self._cfg.PRETRAINED_TRAINABLE)
-            self._rpn = RPN(self._num_anchors, debug=self._debug)
-            if self._with_rcnn:
-                self._roi_pool = ROIPoolingLayer(debug=self._debug)
-                self._rcnn = RCNN(self._num_classes, debug=self._debug)
-
-    def _build(self, image, gt_boxes, is_training=True):
+    def _build(self, image, pretrained_feature_map, gt_boxes, is_training=True):
         """
         Returns bounding boxes and classification probabilities.
 
@@ -83,11 +62,15 @@ class FasterRCNN(snt.AbstractModule):
                 we have (x1, y1, x2, y2)
         """
 
-        image_shape = tf.shape(image)[1:3]
-        pretrained_dict = self._pretrained(image, is_training=is_training)
-        pretrained_feature_map = pretrained_dict['net']
+        self._rpn = RPN(self._num_anchors, debug=self._debug)
+        if self._with_rcnn:
+            self._roi_pool = ROIPoolingLayer(debug=self._debug)
+            self._rcnn = RCNN(self._num_classes, debug=self._debug)
 
-        variable_summaries(pretrained_feature_map, 'pretrained_feature_map', ['RPN'])
+        image_shape = tf.shape(image)[1:3]
+
+        variable_summaries(
+            pretrained_feature_map, 'pretrained_feature_map', ['rpn'])
 
         all_anchors = self._generate_anchors(pretrained_feature_map)
         rpn_prediction = self._rpn(
@@ -104,38 +87,40 @@ class FasterRCNN(snt.AbstractModule):
             prediction_dict['image_shape'] = image_shape
             prediction_dict['all_anchors'] = all_anchors
             prediction_dict['gt_boxes'] = gt_boxes
-            prediction_dict['pretrained_dict'] = pretrained_dict
 
         if self._with_rcnn:
-            roi_prediction = self._roi_pool(rpn_prediction['proposals'], pretrained_feature_map, image_shape)
+            roi_prediction = self._roi_pool(
+                rpn_prediction['proposals'], pretrained_feature_map,
+                image_shape
+            )
 
             # TODO: Missing mapping classification_bbox to real coordinates.
             # (and trimming, and NMS?)
-            classification_prediction = self._rcnn(
-                roi_prediction['roi_pool'], rpn_prediction['proposals'], gt_boxes, image_shape)
+            classification_pred = self._rcnn(
+                roi_prediction['roi_pool'], rpn_prediction['proposals'],
+                gt_boxes, image_shape
+            )
 
-            prediction_dict['classification_prediction'] = classification_prediction
+            prediction_dict['classification_prediction'] = classification_pred
             if self._debug:
                 prediction_dict['roi_prediction'] = roi_prediction
 
-
-        # rpn_prediction['proposals_normalized'] = normalize_bboxes(image, rpn_prediction['proposals'])
-
-        tf.summary.image('image', image, max_outputs=20)
-        tf.summary.image(
-            'top_1_rpn_boxes',
-            draw_bboxes(image, rpn_prediction['proposals'], 1), max_outputs=20
-        )
-        tf.summary.image(
-            'top_10_rpn_boxes',
-            draw_bboxes(image, rpn_prediction['proposals'], 10),
-            max_outputs=20
-        )
-        tf.summary.image(
-            'top_20_rpn_boxes',
-            draw_bboxes(image, rpn_prediction['proposals'], 20),
-            max_outputs=20
-        )
+        if is_training and self._debug:
+            tf.summary.image('image', image, max_outputs=20)
+            tf.summary.image(
+                'top_1_rpn_boxes',
+                draw_bboxes(image, rpn_prediction['proposals'], 1), max_outputs=20
+            )
+            tf.summary.image(
+                'top_10_rpn_boxes',
+                draw_bboxes(image, rpn_prediction['proposals'], 10),
+                max_outputs=20
+            )
+            tf.summary.image(
+                'top_20_rpn_boxes',
+                draw_bboxes(image, rpn_prediction['proposals'], 20),
+                max_outputs=20
+            )
 
         return prediction_dict
 
@@ -143,14 +128,17 @@ class FasterRCNN(snt.AbstractModule):
         """
         Compute the joint training loss for Faster RCNN.
         """
-        with self._enter_variable_scope():
+
+        with tf.name_scope('losses'):
             rpn_loss_dict = self._rpn.loss(
                 prediction_dict['rpn_prediction']
             )
 
             # Losses have a weight assigned.
-            rpn_loss_dict['rpn_cls_loss'] = rpn_loss_dict['rpn_cls_loss'] * self._rpn_cls_loss_weight
-            rpn_loss_dict['rpn_reg_loss'] = rpn_loss_dict['rpn_reg_loss'] * self._rpn_reg_loss_weight
+            rpn_loss_dict['rpn_cls_loss'] = (
+                rpn_loss_dict['rpn_cls_loss'] * self._rpn_cls_loss_weight)
+            rpn_loss_dict['rpn_reg_loss'] = (
+                rpn_loss_dict['rpn_reg_loss'] * self._rpn_reg_loss_weight)
 
             prediction_dict['rpn_loss_dict'] = rpn_loss_dict
 
@@ -159,24 +147,44 @@ class FasterRCNN(snt.AbstractModule):
                     prediction_dict['classification_prediction']
                 )
 
-                rcnn_loss_dict['rcnn_cls_loss'] = rcnn_loss_dict['rcnn_cls_loss'] * self._rcnn_cls_loss_weight
-                rcnn_loss_dict['rcnn_reg_loss'] = rcnn_loss_dict['rcnn_reg_loss'] * self._rcnn_reg_loss_weight
+                rcnn_loss_dict['rcnn_cls_loss'] = (
+                    rcnn_loss_dict['rcnn_cls_loss'] *
+                    self._rcnn_cls_loss_weight
+                )
+                rcnn_loss_dict['rcnn_reg_loss'] = (
+                    rcnn_loss_dict['rcnn_reg_loss'] *
+                    self._rcnn_reg_loss_weight
+                )
 
                 prediction_dict['rcnn_loss_dict'] = rcnn_loss_dict
             else:
                 rcnn_loss_dict = {}
 
-            for loss_name, loss_tensor in list(rpn_loss_dict.items()) + list(rcnn_loss_dict.items()):
-                tf.summary.scalar(loss_name, loss_tensor, collections=['Losses'])
+            all_loses_items = (
+                list(rpn_loss_dict.items()) + list(rcnn_loss_dict.items()))
+            for loss_name, loss_tensor in all_loses_items:
+                tf.summary.scalar(
+                    loss_name, loss_tensor,
+                    collections=self._losses_collections
+                )
                 tf.losses.add_loss(loss_tensor)
 
             regularization_loss = tf.losses.get_regularization_loss()
             no_reg_loss = tf.losses.get_total_loss(add_regularization_losses=False)
             total_loss = tf.losses.get_total_loss()
 
-            tf.summary.scalar('total_loss', total_loss, collections=['Losses'])
-            tf.summary.scalar('no_reg_loss', no_reg_loss, collections=['Losses'])
-            tf.summary.scalar('regularization_loss', regularization_loss, collections=['Losses'])
+            tf.summary.scalar(
+                'total_loss', total_loss,
+                collections=self._losses_collections
+            )
+            tf.summary.scalar(
+                'no_reg_loss', no_reg_loss,
+                collections=self._losses_collections
+            )
+            tf.summary.scalar(
+                'regularization_loss', regularization_loss,
+                collections=self._losses_collections
+            )
 
             return total_loss
 
@@ -216,6 +224,9 @@ class FasterRCNN(snt.AbstractModule):
             )
             return all_anchors
 
+    def load_weights(self, checkpoint_file):
+        return self._pretrained.load_weights(checkpoint_file)
+
     @property
     def summary(self):
         """
@@ -223,11 +234,11 @@ class FasterRCNN(snt.AbstractModule):
         Faster R-CNN network.
         """
         summaries = [
-            tf.summary.merge_all(key='Losses'),
-            tf.summary.merge_all(key='RPN'),
+            tf.summary.merge_all(key=self._losses_collections[0]),
+            tf.summary.merge_all(key='rpn'),
         ]
 
         if self._with_rcnn:
-            summaries.append(tf.summary.merge_all(key='RCNN'))
+            summaries.append(tf.summary.merge_all(key='rcnn'))
 
         return tf.summary.merge(summaries)
--- frcnn/train.py
+++ frcnn/train.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import click
 
 from .network import FasterRCNN
+from .pretrained import VGG, ResNetV2
 from .config import Config
 from .dataset import TFRecordDataset
 
@@ -12,15 +13,24 @@ from tensorflow.python.client import timeline
 from .utils.image_vis import *
 
 
+PRETRAINED_MODULES = {
+    'vgg': VGG,
+    'vgg_16': VGG,
+    'resnet': ResNetV2,
+    'resnetv2': ResNetV2,
+}
+
+
 @click.command()
 @click.option('--num-classes', default=20)
-@click.option('--pretrained-net', default='vgg_16', type=click.Choice(['vgg_16']))
+@click.option('--pretrained-net', default='vgg_16', type=click.Choice(PRETRAINED_MODULES.keys()))
 @click.option('--pretrained-weights')
 @click.option('--model-dir', default='models/')
 @click.option('--checkpoint-file')
+@click.option('--pretrained-checkpoint-file')
 @click.option('--ignore-scope')
 @click.option('--log-dir', default='/tmp/frcnn/')
-@click.option('--save-every', default=10)
+@click.option('--save-every', default=100)
 @click.option('--tf-debug', is_flag=True)
 @click.option('--debug', is_flag=True)
 @click.option('--run-name', default='train')
@@ -28,15 +38,18 @@ from .utils.image_vis import *
 @click.option('--no-log', is_flag=True)
 @click.option('--display-every', default=1, type=int)
 @click.option('--random-shuffle', is_flag=True)
+@click.option('--save-timeline', is_flag=True)
 def train(num_classes, pretrained_net, pretrained_weights, model_dir,
-          checkpoint_file, ignore_scope, log_dir, save_every, tf_debug, debug,
-          run_name, with_rcnn, no_log, display_every, random_shuffle):
+          checkpoint_file, pretrained_checkpoint_file, ignore_scope, log_dir,
+          save_every, tf_debug, debug, run_name, with_rcnn, no_log,
+          display_every, random_shuffle, save_timeline):
 
     if debug or tf_debug:
         tf.logging.set_verbosity(tf.logging.DEBUG)
     else:
         tf.logging.set_verbosity(tf.logging.INFO)
 
+    pretrained = PRETRAINED_MODULES[pretrained_net](trainable=False)
     model = FasterRCNN(
         Config, debug=debug, num_classes=num_classes, with_rcnn=with_rcnn
     )
@@ -60,7 +73,8 @@ def train(num_classes, pretrained_net, pretrained_weights, model_dir,
     # Bbox doesn't need a dimension for batch TODO: Necesitamos standarizarlo!
     # train_bboxes = tf.expand_dims(train_bboxes, 0)
 
-    prediction_dict = model(train_image, train_bboxes)
+    pretrained_dict = pretrained(train_image)
+    prediction_dict = model(train_image, pretrained_dict['net'], train_bboxes)
 
     model_variables = snt.get_normalized_variable_map(
         model, tf.GraphKeys.GLOBAL_VARIABLES
@@ -76,17 +90,14 @@ def train(num_classes, pretrained_net, pretrained_weights, model_dir,
 
         partial_saver = tf.train.Saver(var_list=model_variables)
 
-    saver = snt.get_saver(model)
-
-    if pretrained_weights:
-        # TODO: Sucks to call _pretrained from here.
-        load_op = model._pretrained.load_weights(
-            checkpoint_file=pretrained_weights
-        )
-    else:
-        load_op = tf.no_op(name='not_loading_pretrained')
+    load_op = pretrained.load_weights(
+        checkpoint_file=pretrained_weights
+    )
 
     total_loss = model.loss(prediction_dict)
+
+    model_saver = snt.get_saver(model, name='fasterrcnn_saver')
+    pretrained_saver = snt.get_saver(pretrained, name='pretrained_saver')
 
     initial_learning_rate = 0.0001
 
@@ -105,17 +116,11 @@ def train(num_classes, pretrained_net, pretrained_weights, model_dir,
     optimizer = tf.train.MomentumOptimizer(
         learning_rate=learning_rate, momentum=0.9
     )  # TODO: parameter tunning
-    trainable_vars = snt.get_variables_in_scope(model)
-    if not Config.PRETRAINED_TRAINABLE:
-        before_length = len(trainable_vars)
-        trainable_vars = [
-            v for v in trainable_vars
-            if model._pretrained.module_name not in v.name
-        ]
-        tf.logging.info(
-            'Not training {} variables from pretrained module'.format(
-                before_length - len(trainable_vars)
-            ))
+    trainable_vars = snt.get_variables_in_module(model)
+    if Config.PRETRAINED_TRAINABLE:
+        trainable_vars += snt.get_variables_in_module(pretrained)
+    else:
+        tf.logging.info('Not training variables from pretrained module')
 
     grads_and_vars = optimizer.compute_gradients(total_loss, trainable_vars)
 
@@ -149,18 +154,27 @@ def train(num_classes, pretrained_net, pretrained_weights, model_dir,
     ])
 
     with tf.Session() as sess:
+
+        if tf_debug:
+            from tensorflow.python import debug as tf_debug
+            sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+            sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
+
         sess.run(init_op)  # initialized variables
         sess.run(load_op)  # load pretrained weights
 
         # Restore all variables from checkpoint file
         if checkpoint_file:
-            # TODO: We are better than this.
+            # TODO: We are WAY better than this.
 
             # If ignore_scope is set, we don't load those variables from checkpoint.
             if ignore_scope:
                 partial_saver.restore(sess, checkpoint_file)
             else:
-                saver.restore(sess, checkpoint_file)
+                model_saver.restore(sess, checkpoint_file)
+
+        if pretrained_checkpoint_file:
+            pretrained_saver.restore(sess, pretrained_checkpoint_file)
 
         if not no_log:
             writer = tf.summary.FileWriter(
@@ -184,12 +198,12 @@ def train(num_classes, pretrained_net, pretrained_weights, model_dir,
                 )
 
                 (_, summary, train_loss, step, pred_dict, filename,
-                 scale_factor, *_) = sess.run([
-                    train_op, summarizer, total_loss, global_step,
-                    prediction_dict, train_filename, train_scale_factor,
-                    metric_ops
-                    ], run_metadata=run_metadata, options=run_options
-                )
+                 scale_factor, *_) = sess.run(
+                    [
+                        train_op, summarizer, total_loss, global_step,
+                        prediction_dict, train_filename, train_scale_factor,
+                        metric_ops
+                    ], run_metadata=run_metadata, options=run_options)
 
                 if not no_log:
                     writer.add_summary(summary, step)
@@ -215,23 +229,35 @@ def train(num_classes, pretrained_net, pretrained_weights, model_dir,
                         draw_rcnn_reg_batch_errors(pred_dict)
                         draw_object_prediction(pred_dict)
 
-                    fetched_timeline = timeline.Timeline(run_metadata.step_stats)
-                    chrome_trace = fetched_timeline.generate_chrome_trace_format()
-                    with open('timeline_{}.json'.format(step), 'w') as f:
-                        f.write(chrome_trace)
+                    if save_timeline:
+                        run_tmln = timeline.Timeline(
+                            run_metadata.step_stats)
+                        chrome_trace = run_tmln.generate_chrome_trace_format()
+                        with open('timeline_{}.json'.format(step), 'w') as f:
+                            f.write(chrome_trace)
 
                 count_images += 1
 
                 tf.logging.info('train_loss: {}'.format(train_loss))
-                tf.logging.info('step: {}, filename: {}'.format(step, filename))
+                tf.logging.info('step: {}, file: {}'.format(step, filename))
 
                 if not no_log:
                     if step % save_every == 0:
                         # We don't support partial saver.
-                        saver.save(sess, os.path.join(model_dir, model.scope_name), global_step=step)
+                        model_saver.save(
+                            sess,
+                            os.path.join(model_dir, model.scope_name),
+                            global_step=step
+                        )
+                        pretrained_saver.save(
+                            sess,
+                            os.path.join(model_dir, pretrained.scope_name),
+                            global_step=step
+                        )
 
         except tf.errors.OutOfRangeError:
-            tf.logging.info('iter = {}, train_loss = {:.2f}'.format(step, train_loss))
+            tf.logging.info('iter = {}, train_loss = {:.2f}'.format(
+                step, train_loss))
             tf.logging.info('finished training -- epoch limit reached')
             tf.logging.info('count_images = {}'.format(count_images))
         finally:
