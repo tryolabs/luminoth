54deec3aec7a3bcba5c99479037072bca93f87ea
Javier Rey <javirey@gmail.com>
2017-08-10 14:36:56 -0300

    Rewrites anchor reference generation and adds tests for it

--- luminoth/utils/anchors.py
+++ luminoth/utils/anchors.py
@@ -1,105 +1,42 @@
-# --------------------------------------------------------
-# Faster R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick and Sean Bell
-# --------------------------------------------------------
-
 import numpy as np
 
-# Verify that we compute the same anchors as Shaoqing's matlab implementation:
-#
-#    >> load output/rpn_cachedir/faster_rcnn_VOC2007_ZF_stage1_rpn/anchors.mat
-#    >> anchors
-#
-#    anchors =
-#
-#       -83   -39   100    56
-#      -175   -87   192   104
-#      -359  -183   376   200
-#       -55   -55    72    72
-#      -119  -119   136   136
-#      -247  -247   264   264
-#       -35   -79    52    96
-#       -79  -167    96   184
-#      -167  -343   184   360
 
-# array([[ -83.,  -39.,  100.,   56.],
-#        [-175.,  -87.,  192.,  104.],
-#        [-359., -183.,  376.,  200.],
-#        [ -55.,  -55.,   72.,   72.],
-#        [-119., -119.,  136.,  136.],
-#        [-247., -247.,  264.,  264.],
-#        [ -35.,  -79.,   52.,   96.],
-#        [ -79., -167.,   96.,  184.],
-#        [-167., -343.,  184.,  360.]])
+def generate_anchors_reference(base_size, aspect_ratios, scales):
+    """Generate base anchor to be used as reference of generating all anchors.
 
+    Anchors vary only in width and height. Using the base_size and the
+    different ratios we can calculate the wanted widths and heights.
 
-def generate_anchors_reference(base_size, ratios, scales):
+    Scales apply to area of object.
+
+    Args:
+        base_size (int): Base size of the base anchor (square).
+        aspect_ratios: Ratios to use to generate different anchors. The ratio
+            is the value of height / width.
+        scales: Scaling ratios applied to area.
+
+    Returns:
+        anchors: Numpy array with shape (total_aspect_ratios * total_scales, 4)
+            with the corner points of the reference base anchors using the
+            convention (x_min, y_min, x_max, y_max).
     """
-    Generate anchor (reference) windows by enumerating aspect ratios X
-    scales wrt a reference (0, 0, 15, 15) window.
-    """
+    scales_grid, aspect_ratios_grid = np.meshgrid(scales, aspect_ratios)
+    base_scales = scales_grid.reshape(-1)
+    base_aspect_ratios = aspect_ratios_grid.reshape(-1)
 
-    base_anchor = np.array([1, 1, base_size, base_size]) - 1
-    ratio_anchors = _ratio_enum(base_anchor, ratios)
-    anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
-                         for i in range(ratio_anchors.shape[0])])
+    aspect_ratio_sqrts = np.sqrt(base_aspect_ratios)
+    heights = base_scales * aspect_ratio_sqrts * base_size
+    widths = base_scales / aspect_ratio_sqrts * base_size
+
+    # Center point has the same X, Y value.
+    center_xy = (base_size - 1) / 2.0
+
+    # Create anchor reference.
+    anchors = np.column_stack([
+        center_xy - (widths - 1) / 2,
+        center_xy - (heights - 1) / 2,
+        center_xy + (widths - 1) / 2,
+        center_xy + (heights - 1) / 2,
+    ])
+
     return anchors
-
-
-def _whctrs(anchor):
-    """
-    Return width, height, x center, and y center for an anchor (window).
-    """
-    w = anchor[2] - anchor[0] + 1
-    h = anchor[3] - anchor[1] + 1
-    x_ctr = anchor[0] + 0.5 * (w - 1)
-    y_ctr = anchor[1] + 0.5 * (h - 1)
-    return w, h, x_ctr, y_ctr
-
-
-def _mkanchors(ws, hs, x_ctr, y_ctr):
-    """
-    Given a vector of widths (ws) and heights (hs) around a center
-    (x_ctr, y_ctr), output a set of anchors (windows).
-    """
-    ws = ws[:, np.newaxis]
-    hs = hs[:, np.newaxis]
-    anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
-                         y_ctr - 0.5 * (hs - 1),
-                         x_ctr + 0.5 * (ws - 1),
-                         y_ctr + 0.5 * (hs - 1)))
-    return anchors
-
-
-def _ratio_enum(anchor, ratios):
-    """
-    Enumerate a set of anchors for each aspect ratio wrt an anchor.
-    """
-    w, h, x_ctr, y_ctr = _whctrs(anchor)
-    size = w * h
-    size_ratios = size / ratios
-    ws = np.sqrt(size_ratios)  # np.round(np.sqrt(size_ratios))
-    hs = ws * ratios  # np.round(ws * ratios)
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
-
-
-def _scale_enum(anchor, scales):
-    """
-    Enumerate a set of anchors for each scale wrt an anchor.
-    """
-    w, h, x_ctr, y_ctr = _whctrs(anchor)
-    ws = w * scales
-    hs = h * scales
-    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
-    return anchors
-
-
-if __name__ == '__main__':
-    import time
-    t = time.time()
-    a = generate_anchors_reference(
-        base_size=16, ratios=[0.5, 1, 2], scales=2**np.arange(3, 6)
-    )
