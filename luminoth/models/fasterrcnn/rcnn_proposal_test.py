5ff1ebb3b5ea550e5db92cea567d7bed82e95020
IanTayler <iangtayler@gmail.com>
2017-08-14 18:01:11 -0300

    Add more tests for RCNNProposal. Resolves #36

--- luminoth/models/fasterrcnn/rcnn_proposal_test.py
+++ luminoth/models/fasterrcnn/rcnn_proposal_test.py
@@ -13,7 +13,7 @@ class RCNNProposalTest(tf.test.TestCase):
         self._num_classes = 3
         self._batch_number = 1
 
-        self._image_shape = (1440, 900)
+        self._image_shape = (900, 1440)
         self._config = EasyDict({
             'class_max_detections': 100,
             'class_nms_threshold': 0.6,
@@ -37,7 +37,7 @@ class RCNNProposalTest(tf.test.TestCase):
             return sess.run(graph)
 
     def _get_bbox_pred(self, proposed_boxes, gt_boxes_per_class):
-        """Computes valid bbox_preds from proposals and gt_boxes for each class.
+        """Computes valid bbox_pred from proposals and gt_boxes for each class.
 
         Arguments:
             proposed_boxes: Tensor with shape (num_proposals, 5).
@@ -48,12 +48,27 @@ class RCNNProposalTest(tf.test.TestCase):
             A tensor with shape (num_proposals, num_classes * 4), holding the
             correct bbox_preds.
         """
+
         def bbox_encode(gt_boxes):
-            return encode(proposed_boxes[:, 1:], gt_boxes)
+            return encode(proposed_boxes[:, 1:],
+                          gt_boxes)
         bbox_pred_tensor = tf.map_fn(bbox_encode, gt_boxes_per_class,
                                      dtype=tf.float32)
+        # We need to explicitly unstack the tensor so that tf.concat works
+        # properly.
         bbox_pred_list = tf.unstack(bbox_pred_tensor)
         return tf.concat(bbox_pred_list, 1)
+
+    def _check_proposals_are_clipped(self, proposals, image_shape):
+        """Asserts that no proposals exceed the image boundaries.
+        """
+        for proposal in proposals:
+            self.assertLess(proposal[0], image_shape[1])
+            self.assertLess(proposal[1], image_shape[0])
+            self.assertLess(proposal[2], image_shape[1])
+            self.assertLess(proposal[3], image_shape[0])
+            for i in range(4):
+                self.assertGreaterEqual(proposal[i], 0)
 
     def testBackgroundFilter(self):
         """Tests that we're not returning an object when a proposal is background.
@@ -62,6 +77,7 @@ class RCNNProposalTest(tf.test.TestCase):
         proposal, and one in which all proposals are background. We use the
         same proposed_boxes and gt_boxes, but change the cls_prob.
         """
+
         proposed_boxes = tf.constant([
             (self._batch_number, 85, 500, 730, 590),
             (self._batch_number, 50, 500, 70, 530),
@@ -96,30 +112,195 @@ class RCNNProposalTest(tf.test.TestCase):
             bbox_pred,
             cls_prob_all_background,
         )
-        # One foregound assertion.
+        # Assertion for 'one foreground' case.
         # This assertion has two purposes:
         #     1. checking that we only get one object.
-        #     2. checking that that object is of class 2.
+        #     2. checking that that object has the same box as class 2.
         # We take this to mean we're correctly ignoring the two proposals
         # where 'background' is the highest probability class.
         self.assertAllClose(proposal_prediction_one_foreground['objects'],
                             self._compute_tf_graph(gt_boxes_per_class)[2],
                             atol=self._equality_delta)
-        # All background assertion.
+        # Assertion for 'all background' case.
         self.assertEqual(len(proposal_prediction_all_background['objects']),
                          0)
 
     def testNMSFilter(self):
-        pass
+        """Tests that we're applying NMS correctly.
+        """
+
+        # The first two boxes have a very high IoU between them. One of them
+        # should be filtered by the NMS filter.
+        proposed_boxes = tf.constant([
+            (self._batch_number, 85, 500, 730, 590),
+            (self._batch_number, 50, 500, 740, 570),
+            (self._batch_number, 700, 570, 740, 598),
+        ])
+        gt_boxes_per_class = tf.constant([
+            [(101, 101, 201, 249)],
+            [(200, 502, 209, 532)],
+            [(86, 571, 743, 599)],
+        ])
+        bbox_pred = self._get_bbox_pred(proposed_boxes, gt_boxes_per_class)
+        cls_prob = tf.constant([
+            (0., .1, .3, .6),
+            (.1, .2, .25, .45),
+            (.2, .3, .25, .25),
+        ])
+
+        proposal_prediction = self._run_rcnn_proposal(
+            self._shared_model,
+            proposed_boxes,
+            bbox_pred,
+            cls_prob,
+        )
+        labels = proposal_prediction['proposal_label']
+
+        # Assertions
+        self.assertEqual(proposal_prediction['objects'].shape[0], 2)
+        self.assertIn(0, labels)
+        self.assertIn(2, labels)
 
     def testImageClipping(self):
-        pass
+        """Tests that we're clipping images correctly.
+
+        We test two image shapes, (1440, 900) and (900, 1440). Note we pass
+        shapes as (height, width).
+        """
+
+        proposed_boxes = tf.constant([
+            (self._batch_number, 1300, 800, 1435, 870),
+            (self._batch_number, 10, 1, 30, 7),
+            (self._batch_number, 2, 870, 80, 898),
+        ])
+        gt_boxes_per_class = tf.constant([
+            [(1320, 815, 1455, 912)],
+            [(5, -8, 31, 8)],
+            [(-120, 910, 78, 1040)],
+        ])
+        bbox_pred = self._get_bbox_pred(proposed_boxes, gt_boxes_per_class)
+        cls_prob = tf.constant([
+            (0., 1., 0., 0.),
+            (.2, .25, .3, .25),
+            (.45, 0., 0., .55),
+        ])
+
+        shape1 = (1440, 900)
+        shape2 = (900, 1440)
+
+        proposal_prediction_shape1 = self._run_rcnn_proposal(
+            self._shared_model,
+            proposed_boxes,
+            bbox_pred,
+            cls_prob,
+            image_shape=shape1,
+        )
+        proposal_prediction_shape2 = self._run_rcnn_proposal(
+            self._shared_model,
+            proposed_boxes,
+            bbox_pred,
+            cls_prob,
+            image_shape=shape2,
+        )
+        # Assertions
+        self._check_proposals_are_clipped(
+            proposal_prediction_shape1['objects'],
+            shape1,
+        )
+        self._check_proposals_are_clipped(
+            proposal_prediction_shape2['objects'],
+            shape2,
+        )
 
     def testBboxPred(self):
-        pass
+        """Tests that we're using bbox_pred correctly.
+        """
 
-    def testTotals(self):
-        pass
+        proposed_boxes = tf.constant([
+            (self._batch_number, 200, 315, 400, 370),
+            (self._batch_number, 56, 0, 106, 4),
+            (self._batch_number, 15, 15, 20, 20),
+        ])
+
+        gt_boxes_per_class = tf.constant([
+            [(0, 0, 1, 1)],
+            [(5, 5, 10, 10)],
+            [(15, 15, 20, 20)],
+        ])
+        bbox_pred = self._get_bbox_pred(proposed_boxes, gt_boxes_per_class)
+
+        cls_prob = tf.constant([
+            (0., 1., 0., 0.),
+            (.2, .25, .3, .25),
+            (.45, 0., 0., .55),
+        ])
+
+        proposal_prediction = self._run_rcnn_proposal(
+            self._shared_model,
+            proposed_boxes,
+            bbox_pred,
+            cls_prob,
+        )
+        # Assertions
+        for i in range(self._num_classes):
+            self.assertAllClose(
+                proposal_prediction['objects'][i],
+                self._compute_tf_graph(gt_boxes_per_class)[i][0],
+                atol=self._equality_delta
+            )
+
+    def testLimits(self):
+        """Tests that we're respecting the limits imposed by the config.
+        """
+
+        limits_config = self._config.copy()
+        limits_config['class_max_detections'] = 2
+        limits_config['total_max_detections'] = 3
+        limits_config = EasyDict(limits_config)
+        limits_num_classes = 2
+        limits_model = RCNNProposal(limits_num_classes, limits_config)
+
+        proposed_boxes = tf.constant([
+            (self._batch_number, 0, 0, 1, 1),  # class 0
+            (self._batch_number, 5, 5, 10, 10),  # class 1
+            (self._batch_number, 15, 15, 20, 20),  # class 1
+            (self._batch_number, 25, 25, 30, 30),  # class 0
+            (self._batch_number, 35, 35, 40, 40),
+            (self._batch_number, 38, 40, 65, 65),
+            (self._batch_number, 70, 50, 90, 90),  # class 0
+            (self._batch_number, 95, 95, 100, 100),
+            (self._batch_number, 105, 105, 110, 110),  # class 1
+        ])
+        # All zeroes for our bbox_pred.
+        bbox_pred = tf.constant([[0.] * limits_num_classes * 4] * 9)
+        cls_prob = tf.constant([
+            (0., 1., 0.),
+            (0., .2, .8),
+            (0., .45, .55),
+            (0., .55, .45),
+            (1., 0., 0.),
+            (1., 0., 0.),
+            (0., .95, .05),
+            (1., 0., 0.),
+            (0., .495, .505),
+        ])
+
+        proposal_prediction = self._run_rcnn_proposal(
+            limits_model,
+            proposed_boxes,
+            bbox_pred,
+            cls_prob,
+        )
+        labels = proposal_prediction['proposal_label']
+        num_total = labels.shape[0]
+        num_class0 = labels[labels == 0].shape[0]
+        num_class1 = labels[labels == 1].shape[0]
+
+        self.assertLessEqual(num_class0, limits_config.class_max_detections)
+        self.assertLessEqual(num_class1, limits_config.class_max_detections)
+        # TODO: uncomment the following line when we implement total number
+        # restrictions. Right now we're failing this assert:
+        # self.assertLessEqual(num_total, limits_config.total_max_detections)
 
 
 if __name__ == '__main__':
