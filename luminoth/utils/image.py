a34a4c05d86dfff45e4808edf8701eab56c75d20
Javier Rey <javirey@gmail.com>
2017-08-17 12:03:23 -0300

    Adds better type handling in image resize

--- luminoth/utils/image.py
+++ luminoth/utils/image.py
@@ -30,13 +30,13 @@ def resize_image(image, bboxes=None, min_size=None, max_size=None):
     height = image_shape[0]
     width = image_shape[1]
 
-    if min_size:
+    if min_size is not None:
         # We calculate the upscale factor, the rate we need to use to end up
         # with an image with it's lowest dimension at least `image_min_size`.
         # In case of being big enough the scale factor is 1. (no change)
+        min_size = tf.to_float(min_size)
         min_dimension = tf.minimum(height, width)
-        image_min_size = tf.constant(min_size, tf.float32)
-        upscale_factor = tf.maximum(image_min_size / min_dimension, 1.)
+        upscale_factor = tf.maximum(min_size / min_dimension, 1.)
     else:
         upscale_factor = 1.
 
@@ -44,28 +44,28 @@ def resize_image(image, bboxes=None, min_size=None, max_size=None):
         # We do the same calculating the downscale factor, to end up with an
         # image where the biggest dimension is less than `image_max_size`.
         # When the image is small enough the scale factor is 1. (no change)
+        max_size = tf.to_float(max_size)
         max_dimension = tf.maximum(height, width)
-        image_max_size = tf.constant(max_size, tf.float32)
-        downscale_factor = tf.minimum(image_max_size / max_dimension, 1.)
+        downscale_factor = tf.minimum(max_size / max_dimension, 1.)
     else:
         downscale_factor = 1.
 
     scale_factor = upscale_factor * downscale_factor
 
     # New size is calculate using the scale factor and rounding to int.
-    new_height = tf.to_int32(height * scale_factor)
-    new_width = tf.to_int32(width * scale_factor)
+    new_height = height * scale_factor
+    new_width = width * scale_factor
 
     # Resize image using TensorFlow's own `resize_image` utility.
     image = tf.image.resize_images(
-        image, tf.stack([new_height, new_width]),
+        image, tf.stack(tf.to_int32([new_height, new_width])),
         method=tf.image.ResizeMethod.BILINEAR
     )
 
     if bboxes is not None:
         # We normalize bounding boxes points before modifying the image.
         bboxes_float = tf.to_float(bboxes)
-        x_min, y_min, x_max, y_max, label = tf.split(bboxes_float, 5, axis=1)
+        x_min, y_min, x_max, y_max, label = tf.unstack(bboxes_float, axis=1)
 
         x_min = x_min / width
         y_min = y_min / height
@@ -80,7 +80,7 @@ def resize_image(image, bboxes=None, min_size=None, max_size=None):
         label = tf.to_int32(label)  # Cast back to int.
 
         # Concat points and label to return a [num_bboxes, 5] tensor.
-        bboxes = tf.concat([x_min, y_min, x_max, y_max, label], axis=1)
+        bboxes = tf.stack([x_min, y_min, x_max, y_max, label], axis=1)
         return {
             'image': image,
             'bboxes': bboxes,
