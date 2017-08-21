3c3ffe5a4341d855ed94900ce1f5e3e53f383772
Javier Rey <javirey@gmail.com>
2017-08-21 15:22:15 -0400

    Updates to Sonnet v1.10

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         'easydict==1.7',
         'lxml==3.8.0',
         'tensorflow==1.2.1',
-        'dm-sonnet==1.9',
+        'dm-sonnet==1.10',
         'google-api-python-client==1.6.2',
         'google-cloud-storage==1.2.0',
         'Flask==0.12.2',
