9c960521f46bc593a6dd69bd38c51d2a027867da
Alan Descoins <dekked@gmail.com>
2017-08-24 14:46:52 -0300

    Increment dev version since previous was deleted from test PyPI.

--- luminoth/__init__.py
+++ luminoth/__init__.py
@@ -1,6 +1,6 @@
 from luminoth.cli import cli  # noqa
 
-__version__ = '0.0.1.dev0'
+__version__ = '0.0.1.dev1'
 
 __title__ = 'Luminoth'
 __description__ = 'Computer vision toolkit based on TensorFlow'
