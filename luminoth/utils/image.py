334b414d107f32a655149855830565280b39a459
IanTayler <iangtayler@gmail.com>
2017-09-04 12:52:11 -0300

    PR Fixes.

--- luminoth/utils/image.py
+++ luminoth/utils/image.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 
-from easydict import EasyDict
-
 from luminoth.utils.bbox_transform_tf import clip_boxes
 
 
@@ -116,138 +114,57 @@ def resize_image(image, bboxes=None, min_size=None, max_size=None):
     }
 
 
-def flip_image(image, bboxes=None, left_right=True, up_down=False):
-    """Flips image on its axis for data augmentation.
+def patch_image(image, bboxes=None, offset_height=0, offset_width=0,
+                target_height=None, target_width=None, seed=None):
+    """Gets a patch using tf.image.crop_to_bounding_box and adjusts bboxes
+
+    If patching would leave us with zero bboxes, we return the image and bboxes
+    unchanged.
 
     Args:
-        image: Tensor with image of shape (H, W, 3).
-        config: EasyDict
-            left_right: Boolean flag to flip the image horizontally
-                (left to right).
-            up_down: Boolean flag to flip the image vertically (upside down)
-        bboxes: Optional Tensor with bounding boxes with shape
-            (total_bboxes, 5).
-    Returns:
-        image: Flipped image with the same shape.
-        bboxes: Tensor with the same shape.
-    """
-    image_shape = tf.shape(image)
-    height = image_shape[0]
-    width = image_shape[1]
-
-    if bboxes is not None:
-        # bboxes usually come from dataset as ints, but just in case we are
-        # using flip for preprocessing, where bboxes usually are represented as
-        # floats, we cast them.
-        bboxes = tf.to_int32(bboxes)
-
-    if left_right:
-        image = tf.image.flip_left_right(image)
-        if bboxes is not None:
-            x_min, y_min, x_max, y_max, label = tf.unstack(bboxes, axis=1)
-            new_x_min = width - x_max - 1
-            new_y_min = y_min
-            new_x_max = new_x_min + (x_max - x_min)
-            new_y_max = y_max
-            bboxes = tf.stack(
-                [new_x_min, new_y_min, new_x_max, new_y_max, label], axis=1
-            )
-
-    if up_down:
-        image = tf.image.flip_up_down(image)
-        if bboxes is not None:
-            x_min, y_min, x_max, y_max, label = tf.unstack(bboxes, axis=1)
-            new_x_min = x_min
-            new_y_min = height - y_max - 1
-            new_x_max = x_max
-            new_y_max = new_y_min + (y_max - y_min)
-            bboxes = tf.stack(
-                [new_x_min, new_y_min, new_x_max, new_y_max, label], axis=1
-            )
-
-    return_dict = {'image': image}
-    if bboxes is not None:
-        return_dict['bboxes'] = bboxes
-
-    return return_dict
-
-
-def random_patch(image, bboxes=None, debug=False, min_height=400,
-                 min_width=400):
-    """Gets a random patch from an image.
-
-    Args:
-        image: Tensor with shape (H, W, 3).
-        config: EasyDict
-            min_height: Minimum height of the patch.
-            min_width: Minimum width of the patch.
+        image: Float32 Tensor with shape (H, W, 3).
         bboxes: Tensor with the ground-truth boxes. Shaped (total_boxes, 5).
             The last element in each box is the category label.
-        debug: Boolean. If True, random seeds will be set to 0.
+        offset_height: Height of the upper-left corner of the patch with
+            respect to the original image. Non-negative.
+        offset_width: Width of the upper-left corner of the patch with respect
+            to the original image. Non-negative.
+        target_height: Height of the patch. If set to none, it will be the
+            maximum (tf.shape(image)[0] - offset_height - 1). Positive.
+        target_width: Width of the patch. If set to none, it will be the
+            maximum (tf.shape(image)[1] - offset_width - 1). Positive.
 
     Returns:
-        image: Tensor with shape (H', W', 3), with H' <= H and W' <= W. A
-            random patch of the input image.
-        bboxes: Tensor with shape (new_total_boxes, 5), where we keep
-            bboxes that have their center inside the patch, cropping
-            them to the patch boundaries. If we didn't get any bboxes, then
-            it's set as -1.
+        image: Patch of the original image.
+        bboxes: Adjusted bboxes (only those whose centers are inside the
+            patch). The key isn't set if bboxes is None.
     """
-    if debug:
-        seed = 0
-    else:
-        seed = None
-    # See the documentation on tf.image.crop_to_bounding_box for the meaning of
-    # these variables.
-    offset_width = tf.random_uniform(
-        shape=[],
-        minval=0,
-        maxval=tf.subtract(
-            tf.shape(image)[1],
-            min_width
-        ),
-        dtype=tf.int32,
-        seed=seed
-    )
-    offset_height = tf.random_uniform(
-        shape=[],
-        minval=0,
-        maxval=tf.subtract(
-            tf.shape(image)[0],
-            min_height
-        ),
-        dtype=tf.int32,
-        seed=seed
-    )
-    target_width = tf.random_uniform(
-        shape=[],
-        minval=min_width,
-        maxval=tf.subtract(
-            tf.shape(image)[1],
-            offset_width
-        ),
-        dtype=tf.int32,
-        seed=seed
-    )
-    target_height = tf.random_uniform(
-        shape=[],
-        minval=min_height,
-        maxval=tf.subtract(
-            tf.shape(image)[0],
-            offset_height
-        ),
-        dtype=tf.int32,
-        seed=seed
-    )
+    # TODO: make this function safe with respect to senseless inputs (i.e
+    # having an offset_height that's larger than tf.shape(image)[0], etc.)
+    # As of now we only use it inside random_patch, which already makes sure
+    # the arguments are legal.
+    im_shape = tf.shape(image)
+    if target_height is None:
+        target_height = (im_shape[0] - offset_height - 1)
+    if target_width is None:
+        target_width = (im_shape[1] - offset_width - 1)
+
     new_image = tf.image.crop_to_bounding_box(
         image,
         offset_height=offset_height, offset_width=offset_width,
         target_height=target_height, target_width=target_width
     )
+    patch_shape = tf.shape(new_image)
 
     # Return if we didn't have bboxes.
     if bboxes is None:
-        return_dict = {'image': new_image}
+        # Resize the patch to the original image's size. This is to make sure
+        # we respect restrictions in image size in the models.
+        new_image_resized = tf.image.resize_images(
+            new_image, im_shape[:2],
+            method=tf.image.ResizeMethod.BILINEAR
+        )
+        return_dict = {'image': new_image_resized}
         return return_dict
 
     # Now we will remove all bboxes whose centers are not inside the cropped
@@ -324,57 +241,201 @@ def random_patch(image, bboxes=None, debug=False, min_height=400,
             tf.to_int32(
                 clip_boxes(
                     new_bboxes_unclipped,
-                    imshape=tf.shape(new_image)[:2]
+                    imshape=patch_shape[:2]
                 ),
             ),
             masked_bboxes[:, 4:]
         ],
         axis=1
     )
-    update_condition = tf.greater(
-        tf.gather(tf.shape(new_bboxes), 0),
-        0
+    # Now resize the image to the original size and adjust bboxes accordingly
+    new_image_resized = tf.image.resize_images(
+        new_image, im_shape[:2],
+        method=tf.image.ResizeMethod.BILINEAR
+    )
+    # adjust_bboxes requires height and width values with dtype=float32
+    new_bboxes_resized = adjust_bboxes(
+        new_bboxes,
+        old_height=tf.to_float(patch_shape[0]),
+        old_width=tf.to_float(patch_shape[1]),
+        new_height=tf.to_float(im_shape[0]),
+        new_width=tf.to_float(im_shape[1])
+    )
+
+    # Finally, set up the return dict, but only update the image and bboxes if
+    # our patch has at least one bbox in it.
+    update_condition = tf.greater_equal(
+        tf.shape(new_bboxes_resized)[0],
+        1
     )
     return_dict = {}
     return_dict['image'] = tf.cond(
         update_condition,
-        lambda: new_image,
+        lambda: new_image_resized,
         lambda: image
     )
     return_dict['bboxes'] = tf.cond(
         update_condition,
-        lambda: new_bboxes,
+        lambda: new_bboxes_resized,
         lambda: bboxes
     )
-    return_dict['bboxes'] = new_bboxes
     return return_dict
 
 
-def random_resize(image, bboxes=None, debug=False, min_size=400,
-                  max_size=980):
+def flip_image(image, bboxes=None, left_right=True, up_down=False):
+    """Flips image on its axis for data augmentation.
+
+    Args:
+        image: Tensor with image of shape (H, W, 3).
+        bboxes: Optional Tensor with bounding boxes with shape
+            (total_bboxes, 5).
+        left_right: Boolean flag to flip the image horizontally
+            (left to right).
+        up_down: Boolean flag to flip the image vertically (upside down)
+    Returns:
+        image: Flipped image with the same shape.
+        bboxes: Tensor with the same shape.
+    """
+    image_shape = tf.shape(image)
+    height = image_shape[0]
+    width = image_shape[1]
+
+    if bboxes is not None:
+        # bboxes usually come from dataset as ints, but just in case we are
+        # using flip for preprocessing, where bboxes usually are represented as
+        # floats, we cast them.
+        bboxes = tf.to_int32(bboxes)
+
+    if left_right:
+        image = tf.image.flip_left_right(image)
+        if bboxes is not None:
+            x_min, y_min, x_max, y_max, label = tf.unstack(bboxes, axis=1)
+            new_x_min = width - x_max - 1
+            new_y_min = y_min
+            new_x_max = new_x_min + (x_max - x_min)
+            new_y_max = y_max
+            bboxes = tf.stack(
+                [new_x_min, new_y_min, new_x_max, new_y_max, label], axis=1
+            )
+
+    if up_down:
+        image = tf.image.flip_up_down(image)
+        if bboxes is not None:
+            x_min, y_min, x_max, y_max, label = tf.unstack(bboxes, axis=1)
+            new_x_min = x_min
+            new_y_min = height - y_max - 1
+            new_x_max = x_max
+            new_y_max = new_y_min + (y_max - y_min)
+            bboxes = tf.stack(
+                [new_x_min, new_y_min, new_x_max, new_y_max, label], axis=1
+            )
+
+    return_dict = {'image': image}
+    if bboxes is not None:
+        return_dict['bboxes'] = bboxes
+
+    return return_dict
+
+
+def random_patch(image, bboxes=None, min_height=600, min_width=600,
+                 seed=None):
+    """Gets a random patch from an image.
+
+    min_height and min_width values will be normalized if they are not possible
+    given the input image's shape. See also patch_image.
+
+    Args:
+        image: Tensor with shape (H, W, 3).
+        bboxes: Tensor with the ground-truth boxes. Shaped (total_boxes, 5).
+            The last element in each box is the category label.
+        min_height: Minimum height of the patch.
+        min_width: Minimum width of the patch.
+        seed: Seed to be used in randomizing functions.
+
+    Returns:
+        image: Tensor with shape (H', W', 3), with H' <= H and W' <= W. A
+            random patch of the input image.
+        bboxes: Tensor with shape (new_total_boxes, 5), where we keep
+            bboxes that have their center inside the patch, cropping
+            them to the patch boundaries. If we didn't get any bboxes, the
+            return dict will not have the 'bboxes' key defined.
+    """
+    # Start by normalizing the arguments.
+    # Our patch can't be larger than the original image.
+    im_shape = tf.shape(image)
+    min_height = tf.minimum(min_height, im_shape[0] - 1)
+    min_width = tf.minimum(min_width, im_shape[1] - 1)
+
+    # Now get the patch using tf.image.crop_to_bounding_box.
+    # See the documentation on tf.image.crop_to_bounding_box or the explanation
+    # in patch_image for the meaning of these variables.
+    offset_width = tf.random_uniform(
+        shape=[],
+        minval=0,
+        maxval=tf.subtract(
+            im_shape[1],
+            min_width
+        ),
+        dtype=tf.int32,
+        seed=seed
+    )
+    offset_height = tf.random_uniform(
+        shape=[],
+        minval=0,
+        maxval=tf.subtract(
+            im_shape[0],
+            min_height
+        ),
+        dtype=tf.int32,
+        seed=seed
+    )
+    target_width = tf.random_uniform(
+        shape=[],
+        minval=min_width,
+        maxval=tf.subtract(
+            im_shape[1],
+            offset_width
+        ),
+        dtype=tf.int32,
+        seed=seed
+    )
+    target_height = tf.random_uniform(
+        shape=[],
+        minval=min_height,
+        maxval=tf.subtract(
+            im_shape[0],
+            offset_height
+        ),
+        dtype=tf.int32,
+        seed=seed
+    )
+    return patch_image(
+        image, bboxes=bboxes,
+        offset_height=offset_height, offset_width=offset_width,
+        target_height=target_height, target_width=target_width,
+        seed=seed
+    )
+
+
+def random_resize(image, bboxes=None, min_size=600, max_size=980,
+                  seed=None):
     """Randomly resizes an image within limits.
 
     Args:
         image: Tensor with shape (H, W, 3)
-        config: EasyDict
-            min_size: minimum side-size of the resized image.
-            max_size: maximum side-size of the resized image.
         bboxes: Tensor with the ground-truth boxes. Shaped (total_boxes, 5).
             The last element in each box is the category label.
-        debug: Boolean. If True, random seeds will be set to 0.
+        min_size: minimum side-size of the resized image.
+        max_size: maximum side-size of the resized image.
+        seed: Seed to be used in randomizing functions.
 
     Returns:
         image: Tensor with shape (H', W', 3), satisfying the following.
             min_size <= H' <= H
             min_size <= W' <= W
-        bboxes: Tensor with the same shape as the input bboxes. Or -1 if we
-            didn't pass any bboxes.
-        scale_factor: Scale factor used to modify the image.
+        bboxes: Tensor with the same shape as the input bboxes, if we had them.
+            Else, this key will not be set.
     """
-    if debug:
-        seed = 0
-    else:
-        seed = None
     im_shape = tf.to_float(tf.shape(image))
     new_size = tf.random_uniform(
         shape=[2],
@@ -405,65 +466,63 @@ def random_resize(image, bboxes=None, debug=False, min_size=400,
     return return_dict
 
 
-def random_distortion(image, bboxes=None, debug=False, brightness=None,
-                      contrast=None, hue=None):
+def random_distortion(image, bboxes=None, brightness=None, contrast=None,
+                      hue=None, saturation=None, seed=None):
     """Photometrically distorts an image.
 
     This includes changing the brightness, contrast and hue.
 
     Args:
         image: Tensor with shape (H, W, 3)
-        config: EasyDict with
-            brightness:
-                enable: Boolean
-                max_delta: non-negative float
-            contrast:
-                enable: Boolean
-                lower: non-negative float
-                upper: non-negative float
-            hue:
-                enable: Boolean
-                max_delta: float in [0, 0.5]
-        debug: Boolean. If True, random seeds will be set to 0.
+        brightness:
+            max_delta: non-negative float
+        contrast:
+            lower: non-negative float
+            upper: non-negative float
+        hue:
+            max_delta: float in [0, 0.5]
+        saturation:
+            lower: non-negative float
+            upper: non-negative float
+        seed: Seed to be used in randomizing functions.
 
     Returns:
         image: Distorted image with the same shape as the input image.
         bboxes: Unchanged bboxes.
     """
-    # Get default values if not set.
-    if brightness is None:
-        brightness = EasyDict({
-            'enable': True,
-            'max_delta': 0.3,
-        })
-    if contrast is None:
-        contrast = EasyDict({
-            'enable': True,
-            'lower': 0.4,
-            'upper': 0.8,
-        })
-    if hue is None:
-        hue = EasyDict({
-            'enable': True,
-            'max_delta': 0.2,
-        })
-    if debug:
-        seed = 0
-    else:
-        seed = None
-
-    if brightness.enable:
+    # Following Andrew Howard (2013). "Some improvements on deep convolutional
+    # neural network based image classification."
+    if brightness is not None:
+        if 'max_delta' not in brightness:
+            brightness.max_delta = 0.3
         image = tf.image.random_brightness(
             image, max_delta=brightness.max_delta, seed=seed
         )
-    if contrast.enable:
+    # Changing contrast, even with parameters close to 1, can lead to
+    # excessively distorted images. Use with care.
+    if contrast is not None:
+        if 'lower' not in contrast:
+            contrast.lower = 0.8
+        if 'upper' not in contrast:
+            contrast.upper = 1.2
         image = tf.image.random_contrast(
             image, lower=contrast.lower, upper=contrast.upper,
             seed=seed
         )
-    if hue.enable:
+    if hue is not None:
+        if 'max_delta' not in hue:
+            hue.max_delta = 0.2
         image = tf.image.random_hue(
             image, max_delta=hue.max_delta, seed=seed
+        )
+    if saturation is not None:
+        if 'lower' not in saturation:
+            saturation.lower = 0.8
+        if 'upper' not in saturation:
+            saturation.upper = 1.2
+        image = tf.image.random_saturation(
+            image, lower=saturation.lower, upper=saturation.upper,
+            seed=seed
         )
     if bboxes is None:
         return_dict = {'image': image}
