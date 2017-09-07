3f1dadf1eaac5c924de557e90c99b4677dc966f4
IanTayler <iangtayler@gmail.com>
2017-09-07 17:52:07 -0300

    Reorganize code for modularity in dataset handling.
    Adds luminoth/utils/dataset.py

---
+++ luminoth/utils/dataset.py
@@ -0,0 +1,59 @@
+import tensorflow as tf
+
+from lxml import etree
+
+
+def node2dict(root):
+    if root.getchildren():
+        val = {}
+        for node in root.getchildren():
+            chkey, chval = node2dict(node)
+            val[chkey] = chval
+    else:
+        val = root.text
+
+    return root.tag, val
+
+
+def read_xml(path):
+    with tf.gfile.GFile(path) as f:
+        root = etree.fromstring(f.read())
+
+    annotations = {}
+    for node in root.getchildren():
+        key, val = node2dict(node)
+        # If `key` is object, it's actually a list.
+        if key == 'object':
+            annotations.setdefault(key, []).append(val)
+        else:
+            annotations[key] = val
+
+    return annotations
+
+
+def read_image(path):
+    with tf.gfile.GFile(path, 'rb') as f:
+        image = f.read()
+    return image
+
+
+def _int64(value):
+    value = [int(value)] if not isinstance(value, list) else value
+    return tf.train.Feature(
+        int64_list=tf.train.Int64List(value=value)
+    )
+
+
+def _bytes(value):
+    value = [value] if not isinstance(value, list) else value
+    return tf.train.Feature(
+        bytes_list=tf.train.BytesList(value=value)
+    )
+
+
+def _string(value):
+    value = [value] if not isinstance(value, list) else value
+    value = [v.encode('utf-8') for v in value]
+    return tf.train.Feature(
+        bytes_list=tf.train.BytesList(value=value)
+    )
