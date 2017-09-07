956ca960e8906ca9ce22b35080e5dcfb306b8c67
IanTayler <iangtayler@gmail.com>
2017-09-07 17:52:07 -0300

    Reorganize code for modularity in dataset handling.
    Adds luminoth/utils/dataset.py

--- luminoth/tools/dataset/voc.py
+++ luminoth/tools/dataset/voc.py
@@ -4,7 +4,9 @@ import os
 import random
 import tensorflow as tf
 
-from lxml import etree
+from luminoth.utils.dataset import (
+    read_xml, read_image, _int64, _string, _bytes
+)
 
 
 DEFAULT_TOTAL_CLASSES = 20
@@ -21,40 +23,6 @@ def read_classes(root):
         classes.add(class_name)
 
     return list(sorted(classes))
-
-
-def node2dict(root):
-    if root.getchildren():
-        val = {}
-        for node in root.getchildren():
-            chkey, chval = node2dict(node)
-            val[chkey] = chval
-    else:
-        val = root.text
-
-    return root.tag, val
-
-
-def read_xml(path):
-    with tf.gfile.GFile(path) as f:
-        root = etree.fromstring(f.read())
-
-    annotations = {}
-    for node in root.getchildren():
-        key, val = node2dict(node)
-        # If `key` is object, it's actually a list.
-        if key == 'object':
-            annotations.setdefault(key, []).append(val)
-        else:
-            annotations[key] = val
-
-    return annotations
-
-
-def read_image(path):
-    with tf.gfile.GFile(path, 'rb') as f:
-        image = f.read()
-    return image
 
 
 def load_split(root, split='train'):
@@ -78,28 +46,6 @@ def get_image_path(root, image_id):
 
 def get_image_annotation(root, image_id):
     return os.path.join(root, 'Annotations', '{}.xml'.format(image_id))
-
-
-def _int64(value):
-    value = [int(value)] if not isinstance(value, list) else value
-    return tf.train.Feature(
-        int64_list=tf.train.Int64List(value=value)
-    )
-
-
-def _bytes(value):
-    value = [value] if not isinstance(value, list) else value
-    return tf.train.Feature(
-        bytes_list=tf.train.BytesList(value=value)
-    )
-
-
-def _string(value):
-    value = [value] if not isinstance(value, list) else value
-    value = [v.encode('utf-8') for v in value]
-    return tf.train.Feature(
-        bytes_list=tf.train.BytesList(value=value)
-    )
 
 
 def image_to_example(data_dir, classes, image_id):
@@ -174,7 +120,7 @@ def image_to_example(data_dir, classes, image_id):
 def voc(data_dir, output_dir, splits, ignore_splits, only_filename,
         limit_examples, limit_classes, seed, debug):
     """
-    Prepare VOC dataset for ingestion.
+    Prepares VOC dataset for ingestion.
 
     Converts the VOC dataset into three (one per split) TFRecords files.
     """
