bae7a37e58414ddf6e8f6f6f5d673e5e5c677550
Javier Rey <javirey@gmail.com>
2017-09-18 19:50:33 -0300

    Remove duplicate scope in RCNN loss

--- luminoth/models/fasterrcnn/rcnn.py
+++ luminoth/models/fasterrcnn/rcnn.py
@@ -257,121 +257,120 @@ class RCNN(snt.AbstractModule):
                     regression task to adjust correctly labeled boxes.
 
         """
-        with self._enter_variable_scope():
-            with tf.name_scope('RCNNLoss'):
-                cls_score = prediction_dict['rcnn']['cls_score']
-                # cls_prob = prediction_dict['rcnn']['cls_prob']
-                # Cast target explicitly as int32.
-                cls_target = tf.cast(
-                    prediction_dict['target']['cls'], tf.int32
+        with tf.name_scope('RCNNLoss'):
+            cls_score = prediction_dict['rcnn']['cls_score']
+            # cls_prob = prediction_dict['rcnn']['cls_prob']
+            # Cast target explicitly as int32.
+            cls_target = tf.cast(
+                prediction_dict['target']['cls'], tf.int32
+            )
+
+            # First we need to calculate the log loss betweetn cls_prob and
+            # cls_target
+
+            # We only care for the targets that are >= 0
+            not_ignored = tf.reshape(tf.greater_equal(
+                cls_target, 0), [-1], name='not_ignored')
+            # We apply boolean mask to score, prob and target.
+            cls_score_labeled = tf.boolean_mask(
+                cls_score, not_ignored, name='cls_score_labeled')
+            # cls_prob_labeled = tf.boolean_mask(
+            #    cls_prob, not_ignored, name='cls_prob_labeled')
+            cls_target_labeled = tf.boolean_mask(
+                cls_target, not_ignored, name='cls_target_labeled')
+
+            tf.summary.scalar(
+                'batch_size',
+                tf.shape(cls_score_labeled)[0], ['rcnn']
+            )
+
+            # Transform to one-hot vector
+            cls_target_one_hot = tf.one_hot(
+                cls_target_labeled, depth=self._num_classes + 1,
+                name='cls_target_one_hot'
+            )
+
+            # We get cross entropy loss of each proposal.
+            cross_entropy_per_proposal = (
+                tf.nn.softmax_cross_entropy_with_logits(
+                    labels=cls_target_one_hot, logits=cls_score_labeled
+                )
+            )
+
+            if self._debug:
+                prediction_dict['_debug']['losses'] = {}
+                # Save the cross entropy per proposal to be able to
+                # visualize proposals with high and low error.
+                prediction_dict['_debug']['losses'][
+                    'cross_entropy_per_proposal'
+                ] = (
+                    cross_entropy_per_proposal
                 )
 
-                # First we need to calculate the log loss betweetn cls_prob and
-                # cls_target
+            # Second we need to calculate the smooth l1 loss between
+            # `bbox_offsets` and `bbox_offsets_target`.
+            bbox_offsets = prediction_dict['rcnn']['bbox_offsets']
+            bbox_offsets_target = (
+                prediction_dict['target']['bbox_offsets']
+            )
 
-                # We only care for the targets that are >= 0
-                not_ignored = tf.reshape(tf.greater_equal(
-                    cls_target, 0), [-1], name='not_ignored')
-                # We apply boolean mask to score, prob and target.
-                cls_score_labeled = tf.boolean_mask(
-                    cls_score, not_ignored, name='cls_score_labeled')
-                # cls_prob_labeled = tf.boolean_mask(
-                #    cls_prob, not_ignored, name='cls_prob_labeled')
-                cls_target_labeled = tf.boolean_mask(
-                    cls_target, not_ignored, name='cls_target_labeled')
+            # We only want the non-background labels bounding boxes.
+            not_ignored = tf.reshape(tf.greater(cls_target, 0), [-1])
+            bbox_offsets_labeled = tf.boolean_mask(
+                bbox_offsets, not_ignored, name='bbox_offsets_labeled')
+            bbox_offsets_target_labeled = tf.boolean_mask(
+                bbox_offsets_target, not_ignored,
+                name='bbox_offsets_target_labeled'
+            )
 
-                tf.summary.scalar(
-                    'batch_size',
-                    tf.shape(cls_score_labeled)[0], ['rcnn']
+            cls_target_labeled = tf.boolean_mask(
+                cls_target, not_ignored, name='cls_target_labeled')
+            # `cls_target_labeled` is based on `cls_target` which has
+            # `num_classes` + 1 classes.
+            # for making `one_hot` with depth `num_classes` to work we need
+            # to lower them to make them 0-index.
+            cls_target_labeled = cls_target_labeled - 1
+
+            cls_target_one_hot = tf.one_hot(
+                cls_target_labeled, depth=self._num_classes,
+                name='cls_target_one_hot'
+            )
+
+            # cls_target now is (num_labeled, num_classes)
+            bbox_flatten = tf.reshape(
+                bbox_offsets_labeled, [-1, 4], name='bbox_flatten')
+
+            # We use the flatten cls_target_one_hot as boolean mask for the
+            # bboxes.
+            cls_flatten = tf.cast(tf.reshape(
+                cls_target_one_hot, [-1]), tf.bool, 'cls_flatten_as_bool')
+
+            bbox_offset_cleaned = tf.boolean_mask(
+                bbox_flatten, cls_flatten, 'bbox_offset_cleaned')
+
+            # Calculate the smooth l1 loss between the "cleaned" bboxes
+            # offsets (that means, the useful results) and the labeled
+            # targets.
+            reg_loss_per_proposal = smooth_l1_loss(
+                bbox_offset_cleaned, bbox_offsets_target_labeled)
+
+            tf.summary.scalar(
+                'rcnn_foreground_samples',
+                tf.shape(bbox_offset_cleaned)[0], ['rcnn']
+            )
+
+            if self._debug:
+                # Also save reg loss per proposals to be able to visualize
+                # good and bad proposals in debug mode.
+                prediction_dict['_debug']['losses'][
+                    'reg_loss_per_proposal'
+                ] = (
+                    reg_loss_per_proposal
                 )
 
-                # Transform to one-hot vector
-                cls_target_one_hot = tf.one_hot(
-                    cls_target_labeled, depth=self._num_classes + 1,
-                    name='cls_target_one_hot'
-                )
-
-                # We get cross entropy loss of each proposal.
-                cross_entropy_per_proposal = (
-                    tf.nn.softmax_cross_entropy_with_logits(
-                        labels=cls_target_one_hot, logits=cls_score_labeled
-                    )
-                )
-
-                if self._debug:
-                    prediction_dict['_debug']['losses'] = {}
-                    # Save the cross entropy per proposal to be able to
-                    # visualize proposals with high and low error.
-                    prediction_dict['_debug']['losses'][
-                        'cross_entropy_per_proposal'
-                    ] = (
-                        cross_entropy_per_proposal
-                    )
-
-                # Second we need to calculate the smooth l1 loss between
-                # `bbox_offsets` and `bbox_offsets_target`.
-                bbox_offsets = prediction_dict['rcnn']['bbox_offsets']
-                bbox_offsets_target = (
-                    prediction_dict['target']['bbox_offsets']
-                )
-
-                # We only want the non-background labels bounding boxes.
-                not_ignored = tf.reshape(tf.greater(cls_target, 0), [-1])
-                bbox_offsets_labeled = tf.boolean_mask(
-                    bbox_offsets, not_ignored, name='bbox_offsets_labeled')
-                bbox_offsets_target_labeled = tf.boolean_mask(
-                    bbox_offsets_target, not_ignored,
-                    name='bbox_offsets_target_labeled'
-                )
-
-                cls_target_labeled = tf.boolean_mask(
-                    cls_target, not_ignored, name='cls_target_labeled')
-                # `cls_target_labeled` is based on `cls_target` which has
-                # `num_classes` + 1 classes.
-                # for making `one_hot` with depth `num_classes` to work we need
-                # to lower them to make them 0-index.
-                cls_target_labeled = cls_target_labeled - 1
-
-                cls_target_one_hot = tf.one_hot(
-                    cls_target_labeled, depth=self._num_classes,
-                    name='cls_target_one_hot'
-                )
-
-                # cls_target now is (num_labeled, num_classes)
-                bbox_flatten = tf.reshape(
-                    bbox_offsets_labeled, [-1, 4], name='bbox_flatten')
-
-                # We use the flatten cls_target_one_hot as boolean mask for the
-                # bboxes.
-                cls_flatten = tf.cast(tf.reshape(
-                    cls_target_one_hot, [-1]), tf.bool, 'cls_flatten_as_bool')
-
-                bbox_offset_cleaned = tf.boolean_mask(
-                    bbox_flatten, cls_flatten, 'bbox_offset_cleaned')
-
-                # Calculate the smooth l1 loss between the "cleaned" bboxes
-                # offsets (that means, the useful results) and the labeled
-                # targets.
-                reg_loss_per_proposal = smooth_l1_loss(
-                    bbox_offset_cleaned, bbox_offsets_target_labeled)
-
-                tf.summary.scalar(
-                    'rcnn_foreground_samples',
-                    tf.shape(bbox_offset_cleaned)[0], ['rcnn']
-                )
-
-                if self._debug:
-                    # Also save reg loss per proposals to be able to visualize
-                    # good and bad proposals in debug mode.
-                    prediction_dict['_debug']['losses'][
-                        'reg_loss_per_proposal'
-                    ] = (
-                        reg_loss_per_proposal
-                    )
-
-                return {
-                    'rcnn_cls_loss': tf.reduce_mean(
-                        cross_entropy_per_proposal
-                    ),
-                    'rcnn_reg_loss': tf.reduce_mean(reg_loss_per_proposal),
-                }
+            return {
+                'rcnn_cls_loss': tf.reduce_mean(
+                    cross_entropy_per_proposal
+                ),
+                'rcnn_reg_loss': tf.reduce_mean(reg_loss_per_proposal),
+            }
