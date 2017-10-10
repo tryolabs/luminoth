32c7b914c0ba2f91d9e58b7bda1c7355a79ac8bc
Alan Descoins <dekked@gmail.com>
2017-10-10 22:38:59 +0100

    Version bump to 0.0.1 for initial PyPI release.

--- luminoth/__init__.py
+++ luminoth/__init__.py
@@ -1,10 +1,10 @@
 from luminoth.cli import cli  # noqa
 
-__version__ = '0.0.1.dev1'
+__version__ = '0.0.1'
 
 __title__ = 'Luminoth'
 __description__ = 'Computer vision toolkit based on TensorFlow'
-__uri__ = 'http://luminoth.ai'
+__uri__ = 'https://luminoth.ai'
 __doc__ = __description__ + ' <' + __uri__ + '>'
 
 __author__ = 'Tryolabs'
