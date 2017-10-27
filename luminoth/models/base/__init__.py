63e88e6ff83b2b69129d9dd3fcbb6fbc1a825373
gastonrod07 <gaston@tryolabs.com>
2017-10-27 15:18:14 -0300

    Adds a Fully Convolutional Reduced network for use in SSD

--- luminoth/models/base/__init__.py
+++ luminoth/models/base/__init__.py
@@ -1,2 +1,3 @@
 from .base_network import BaseNetwork  # noqa
 from .truncated_base_network import TruncatedBaseNetwork  # noqa
+from .fully_convolutional_network import FullyConvolutionalNetwork  # noqa
