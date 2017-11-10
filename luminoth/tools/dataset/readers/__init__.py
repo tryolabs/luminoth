014eda7517040bf2ff52115a77cfba7ed869ec4c
Javier Rey <javirey@gmail.com>
2017-11-09 22:58:38 -0300

    Adds COCO reader for object detection

--- luminoth/tools/dataset/readers/__init__.py
+++ luminoth/tools/dataset/readers/__init__.py
@@ -1,12 +1,15 @@
 from .base_reader import BaseReader, InvalidDataDirectory  # noqa
 from .object_detection import ObjectDetectionReader  # noqa
-from .object_detection import ImageNetReader, PascalVOCReader, CSVReader
+from .object_detection import (
+    ImageNetReader, PascalVOCReader, CSVReader, COCOReader
+)
 
 READERS = {
     'imagenet': ImageNetReader,
     'pascal': PascalVOCReader,
     'voc': PascalVOCReader,
     'csv': CSVReader,
+    'coco': COCOReader,
 }
 
 
