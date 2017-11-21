98c282d05740d168e411d13f15f94c7ca1e2688b
Javier Rey <javirey@gmail.com>
2017-11-21 13:17:53 -0300

    Adds OpenImages reader

--- luminoth/tools/dataset/readers/object_detection/__init__.py
+++ luminoth/tools/dataset/readers/object_detection/__init__.py
@@ -4,4 +4,5 @@ from .coco import COCOReader  # noqa
 from .csv_reader import CSVReader  # noqa
 from .flat_reader import FlatReader  # noqa
 from .imagenet import ImageNetReader  # noqa
+from .openimages import OpenImagesReader  # noqa
 from .pascalvoc import PascalVOCReader  # noqa
