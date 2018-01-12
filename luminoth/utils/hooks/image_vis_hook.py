4d18ed968719c636d785ec24c699225cdd72054d
psoto <psoto23.ps@gmail.com>
2018-01-12 14:47:45 +0000

    SSD: Adapted ImageVisHook to work with SSD (WIP)

--- luminoth/utils/hooks/image_vis_hook.py
+++ luminoth/utils/hooks/image_vis_hook.py
@@ -5,10 +5,9 @@ from luminoth.utils.image_vis import image_vis_summaries
 
 
 class ImageVisHook(tf.train.SessionRunHook):
-    def __init__(self, prediction_dict, image, with_rcnn=True,
-                 gt_bboxes=None, every_n_steps=None, every_n_secs=None,
-                 output_dir=None, summary_writer=None,
-                 image_visualization_mode=None):
+    def __init__(self, prediction_dict, image, config=None, gt_bboxes=None,
+                 every_n_steps=None, every_n_secs=None, output_dir=None,
+                 summary_writer=None, image_visualization_mode=None):
         super(ImageVisHook, self).__init__()
         if (every_n_secs is None) == (every_n_steps is None):
             raise ValueError(
@@ -22,7 +21,7 @@ class ImageVisHook(tf.train.SessionRunHook):
             every_steps=every_n_steps, every_secs=every_n_secs)
 
         self._prediction_dict = prediction_dict
-        self._with_rcnn = with_rcnn
+        self._config = config
         self._output_dir = output_dir
         self._summary_writer = summary_writer
         self._image_visualization_mode = image_visualization_mode
@@ -66,7 +65,7 @@ class ImageVisHook(tf.train.SessionRunHook):
             prediction_dict = results.get('prediction_dict')
             if prediction_dict is not None:
                 summaries = image_vis_summaries(
-                    prediction_dict, with_rcnn=self._with_rcnn,
+                    prediction_dict, config=self._config,
                     image_visualization_mode=self._image_visualization_mode,
                     image=results.get('image'),
                     gt_bboxes=results.get('gt_bboxes')
