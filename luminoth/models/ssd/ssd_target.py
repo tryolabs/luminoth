e8f79d2d0c38d3f151d6d5c5785f0e7f27112d2b
Joaquin Alori <joaquin.alori@gmail.com>
2018-01-25 18:12:39 -0300

    SSD: Refactor target (WIP)

--- luminoth/models/ssd/ssd_target.py
+++ luminoth/models/ssd/ssd_target.py
@@ -28,42 +28,41 @@ class SSDTarget(snt.AbstractModule):
         self._background_threshold_low = config.background_threshold_low
         self._seed = seed
 
-    def _build(self, probs, proposals, gt_boxes, im_shape):
+    def _build(self, probs, all_anchors, gt_boxes, im_shape):
         """
         Args:
-            proposals: A Tensor with the RPN bounding boxes proposals.
-                The shape of the Tensor is (num_proposals, 4).
+            all_anchors: A Tensor with anchors for all of SSD's features.
+                The shape of the Tensor is (num_anchors, 4).
             gt_boxes: A Tensor with the ground truth boxes for the image.
                 The shape of the Tensor is (num_gt, 5), having the truth label
                 as the last value for each box.
             im_shape: (1, height, width, )
         Returns:
-            proposals_label: Either a truth value of the proposals (a value
+            class_targets: Either a truth value of the anchor (a value
                 between 0 and num_classes, with 0 being background), or -1 when
-                the proposal is to be ignored in the minibatch.
-                The shape of the Tensor is (num_proposals, 1).
-            bbox_targets: A bounding box regression target for each of the
-                proposals that have and greater than zero label. For every
-                other proposal we return zeros.
-                The shape of the Tensor is (num_proposals, 4).
+                the anchor is to be ignored in the minibatch.
+                The shape of the Tensor is (num_anchors, 1).
+            bbox_offsets_targets: A bounding box regression target for each of the
+                anchors that have and greater than zero label. For every
+                other anchors we return zeros.
+                The shape of the Tensor is (num_anchors, 4).
         """
 
-        # Proposals = all_anchors
-        proposals = tf.cast(proposals, tf.float32)
+        all_anchors = tf.cast(all_anchors, tf.float32)
         gt_boxes = tf.cast(gt_boxes, tf.float32)
 
-        # We are going to label each proposal based on the IoU with
-        # `gt_boxes`. Start by filling the labels with 0, marking them as
-        # unknown (-2).
-        proposals_label_shape = tf.gather(tf.shape(proposals), [0])
-        proposals_label = tf.fill(
-            dims=proposals_label_shape,
+        # We are going to label each anchor based on the IoU with
+        # `gt_boxes`. Start by filling the labels with -1, marking them as
+        # unknown.
+        anchors_label_shape = tf.gather(tf.shape(all_anchors), [0])
+        anchors_label = tf.fill(
+            dims=anchors_label_shape,
             value=-1.
         )
 
-        overlaps = bbox_overlap_tf(proposals, gt_boxes[:, :4])
-        # overlaps now contains (num_proposals, num_gt_boxes) with the IoU of
-        # proposal P and ground truth box G in overlaps[P, G]
+        overlaps = bbox_overlap_tf(all_anchors, gt_boxes[:, :4])
+        # overlaps now contains (num_anchors, num_gt_boxes) with the IoU of
+        # anchor P and ground truth box G in overlaps[P, G]
 
         # For each overlap there is two possible outcomes for labelling by now:
         #  if max(iou) > config.foreground_threshold then we label with
@@ -72,79 +71,82 @@ class SSDTarget(snt.AbstractModule):
         #       config.background_threshold_high) we label with background (0)
         #  else we label with -1.
 
-        # max_overlaps gets, for each proposal, the index in which we can
+        # max_overlaps gets, for each anchor, the index in which we can
         # find the gt_box with which it has the highest overlap.
         max_overlaps = tf.reduce_max(overlaps, axis=1)
 
-        # Filter proposals with negative or zero area.
-        (x_min, y_min, x_max, y_max) = tf.unstack(
-            proposals, axis=1
-        )
-        proposal_filter = tf.greater(
-            tf.maximum(x_max - x_min, 0.0) * tf.maximum(y_max - y_min, 0.0),
-            0.0
-        )
-        # We (force) reshape the filter so that we can use it as a boolean mask
-        proposal_filter = tf.reshape(proposal_filter, [-1])
-        # Ignore the proposals with negative area with a -1
-        proposals_label = tf.where(
-            condition=proposal_filter,
-            x=proposals_label,
-            y=tf.fill(dims=proposals_label_shape, value=-1.)
-        )
+        # TODO Check if removing this breaks something
+        # # Filter all_anchors with negative or zero area.
+        # (x_min, y_min, x_max, y_max) = tf.unstack(
+        #     all_anchors, axis=1
+        # )
+        # anchors_filter = tf.greater(
+        #     tf.maximum(x_max - x_min, 0.0) * tf.maximum(y_max - y_min, 0.0),
+        #     0.0
+        # )
+        # # We (force) reshape the filter so that we can use it as a boolean mask
+        # anchors_filter = tf.reshape(anchors_filter, [-1])
+        # # Ignore the all_anchors with negative area with a -1
+        # anchors_label = tf.where(
+        #     condition=anchors_filter,
+        #     x=anchors_label,
+        #     y=tf.fill(dims=anchors_label_shape, value=-1.)
+        # )
 
-        # Get the index of the best gt_box for each proposal.
-        overlaps_best_gt_idxs = tf.argmax(overlaps, axis=1)
+        # Get the index of the best gt_box for each anchor.
+        best_gtbox_for_anchors_idx = tf.argmax(overlaps, axis=1)
         # Having the index of the gt bbox with the best label we need to get
-        # the label for each gt box and sum it one because 0 is used for
+        # the label for each gt box and sum 1 to it because 0 is used for
         # background.
-        best_fg_labels_for_proposals = tf.add(
-            tf.gather(gt_boxes[:, 4], overlaps_best_gt_idxs),
+        best_fg_labels_for_anchors = tf.add(
+            tf.gather(gt_boxes[:, 4], best_gtbox_for_anchors_idx),
             1.
         )
         iou_is_fg = tf.greater_equal(
             max_overlaps, self._foreground_threshold
         )
-        best_proposals_idxs = tf.argmax(overlaps, axis=0)
+        # We update anchors_label with the value in
+        # best_fg_labels_for_anchors only when the box is foreground.
+        # TODO: Replace with a sparse_to_dense with -1 default_value
+        anchors_label = tf.where(
+            condition=iou_is_fg,
+            x=best_fg_labels_for_anchors,
+            y=anchors_label
+        )
 
-        # Set the indices in best_proposals_idxs to True, and the rest to
-        # false.
+        # ################### TODO is this necesary? ###############
+        best_anchor_idxs = tf.argmax(overlaps, axis=0)
+        # Set the indices in best_anchor_idxs to True, and the rest to false.
         # tf.sparse_to_dense is used because we know the set of indices which
         # we want to set to True, and we know the rest of the indices
         # should be set to False. That's exactly the use case of
         # tf.sparse_to_dense.
         is_best_box = tf.sparse_to_dense(
-            sparse_indices=tf.reshape(best_proposals_idxs, [-1]),
+            sparse_indices=best_anchor_idxs,
             sparse_values=True, default_value=False,
-            output_shape=tf.cast(proposals_label_shape, tf.int64),
+            output_shape=tf.cast(anchors_label_shape, tf.int64),
             validate_indices=False
         )
-        # We update proposals_label with the value in
-        # best_fg_labels_for_proposals only when the box is foreground.
-        proposals_label = tf.where(
-            condition=iou_is_fg,
-            x=best_fg_labels_for_proposals,
-            y=proposals_label
-        )
-        # Now we need to find the proposals that are the best for each of the
-        # gt_boxes. We overwrite the previous proposals_label with this
-        # because setting the best proposal for each gt_box has priority.
-        best_proposals_gt_labels = tf.sparse_to_dense(
-            sparse_indices=tf.reshape(best_proposals_idxs, [-1]),
+        # Now we need to find the anchors that are the best for each of the
+        # gt_boxes. We overwrite the previous anchors_label with this
+        # because setting the best anchor for each gt_box has priority.
+        best_anchors_gt_labels = tf.sparse_to_dense(
+            sparse_indices=best_anchor_idxs,
             sparse_values=gt_boxes[:, 4] + 1,
             default_value=-1,
-            output_shape=tf.cast(proposals_label_shape, tf.int64),
+            output_shape=tf.cast(anchors_label_shape, tf.int64),
             validate_indices=False,
             name="get_right_labels_for_bestboxes"
         )
-        proposals_label = tf.where(
+        anchors_label = tf.where(
             condition=is_best_box,
-            x=best_proposals_gt_labels,
-            y=proposals_label,
-            name="update_labels_for_bestbox_proposals"
+            x=best_anchors_gt_labels,
+            y=anchors_label,
+            name="update_labels_for_bestbox_anchors"
         )
+        #  ##########################################################
 
-        # Use the worst backgrounds (the bgs which probability of being fg is
+        # Use the worst backgrounds (the bgs whose probability of being fg is
         # the greatest).
         cls_probs = probs[:, 1:]
         max_cls_probs = tf.reduce_max(cls_probs, axis=1)
@@ -157,12 +159,12 @@ class SSDTarget(snt.AbstractModule):
         max_cls_probs = tf.where(
             condition=bg_overlaps_filter,
             x=max_cls_probs,
-            y=tf.fill(dims=proposals_label_shape, value=-1.),
+            y=tf.fill(dims=anchors_label_shape, value=-1.),
         )
 
         # We calculate up to how many backgrounds we desire based on the
         # final number of foregrounds and the hard minning ratio.
-        num_fg_mask = tf.greater(proposals_label, 0.0)
+        num_fg_mask = tf.greater(anchors_label, 0.0)
         num_fg = tf.cast(tf.count_nonzero(num_fg_mask), tf.float32)
 
         num_bg = tf.cast(num_fg * self._foreground_fraction, tf.int32)
@@ -171,57 +173,57 @@ class SSDTarget(snt.AbstractModule):
         set_bg = tf.sparse_to_dense(
             sparse_indices=top_k_bg.indices,
             sparse_values=True, default_value=False,
-            output_shape=proposals_label_shape,
+            output_shape=anchors_label_shape,
             validate_indices=False
         )
 
-        proposals_label = tf.where(
+        anchors_label = tf.where(
             condition=set_bg,
-            x=tf.fill(dims=proposals_label_shape, value=0.),
-            y=proposals_label
+            x=tf.fill(dims=anchors_label_shape, value=0.),
+            y=anchors_label
         )
 
         """
-        Next step is to calculate the proper targets for the proposals labeled
-        based on the values of the ground-truth boxes.
-        We have to use only the proposals labeled >= 1, each matching with
+        Next step is to calculate the proper bbox targets for the labeled
+        anchors based on the values of the ground-truth boxes.
+        We have to use only the anchors labeled >= 1, each matching with
         the proper gt_boxes
         """
 
-        # Get the ids of the proposals that matter for bbox_target comparisson.
-        is_proposal_with_target = tf.greater(
-            proposals_label, 0
+        # Get the ids of the anchors that mater for bbox_target comparison.
+        is_anchor_with_target = tf.greater(
+            anchors_label, 0
         )
-        proposals_with_target_idx = tf.where(
-            condition=is_proposal_with_target
+        anchors_with_target_idx = tf.where(
+            condition=is_anchor_with_target
         )
-        # Get the corresponding ground truth box only for the proposals with
+        # Get the corresponding ground truth box only for the anchors with
         # target.
         gt_boxes_idxs = tf.gather(
-            overlaps_best_gt_idxs,
-            proposals_with_target_idx
+            best_gtbox_for_anchors_idx,
+            anchors_with_target_idx
         )
         # Get the values of the ground truth boxes.
-        proposals_gt_boxes = tf.gather_nd(
+        anchors_gt_boxes = tf.gather_nd(
             gt_boxes[:, :4], gt_boxes_idxs
         )
-        # We create the same array but with the proposals
-        proposals_with_target = tf.gather_nd(
-            proposals,
-            proposals_with_target_idx
+        # We create the same array but with the anchors
+        anchors_with_target = tf.gather_nd(
+            all_anchors,
+            anchors_with_target_idx
         )
         # We create our targets with bbox_transform
-        bbox_targets_nonzero = encode(
-            proposals_with_target,
-            proposals_gt_boxes,
+        bbox_targets = encode(
+            anchors_with_target,
+            anchors_gt_boxes,
         )
 
-        # We unmap targets to proposal_labels (containing the length of
-        # proposals)
+        # We unmap targets to anchor_labels (containing the length of
+        # anchors)
         bbox_targets = tf.scatter_nd(
-            indices=proposals_with_target_idx,
-            updates=bbox_targets_nonzero,
-            shape=tf.cast(tf.shape(proposals), tf.int64)
+            indices=anchors_with_target_idx,
+            updates=bbox_targets,
+            shape=tf.cast(tf.shape(all_anchors), tf.int64)
         )
 
-        return proposals_label, bbox_targets
+        return anchors_label, bbox_targets
