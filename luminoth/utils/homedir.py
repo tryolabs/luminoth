208f1963dd50fa494611c33c2fcbaf6cf3f6dbd0
Agustín Azzinnari <agustin@tryolabs.com>
2018-02-28 15:29:37 -0300

    Refactor path management for Luminoth's home

---
+++ luminoth/utils/homedir.py
@@ -0,0 +1,20 @@
+"""Luminoth home (~/.luminoth) management utilities."""
+import os
+
+
+DEFAULT_LUMINOTH_HOME = os.path.expanduser('~/.luminoth')
+
+
+def get_luminoth_home(create_if_missing=True):
+    """Returns Luminoth's homedir."""
+    # Get Luminoth's home directory (the default one or the overridden).
+    path = DEFAULT_LUMINOTH_HOME
+    if 'LUMI_HOME' in os.environ:
+        path = os.environ['LUMI_HOME']
+    path = os.path.abspath(path)
+
+    # Create the directory if it doesn't exist.
+    if create_if_missing and not os.path.exists(path):
+        os.makedirs(path, exist_ok=True)
+
+    return path
