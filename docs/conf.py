9f7bb6d08aae447cb071078441f539fe2a93af1a
Agustín Azzinnari <agustin@tryolabs.com>
2018-04-18 12:26:45 -0300

    Fix documentation build
    * Remove sys.path modification in docs conf
    * Add a docs/requirements.txt to make readthedocs use Tensorflow
    1.5.0, as newer versions throw a segfault. (Temporary fix.)

--- docs/conf.py
+++ docs/conf.py
@@ -1,22 +1,8 @@
-# -*- coding: utf-8 -*-
 #
 # Configuration file for the Sphinx documentation builder.
 #
-# This file does only contain a selection of the most common options. For a
-# full list see the documentation:
-# http://www.sphinx-doc.org/en/stable/config
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
 import pkg_resources
 import sys
-
-sys.path.insert(0, os.path.abspath('..'))
 
 
 # -- Project information -----------------------------------------------------
